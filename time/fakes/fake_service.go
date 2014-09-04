a57c09fe96639cc90db4822c0712a36777ea0187
Andrea Law <alaw@pivotal.io>
2014-09-04 16:02:58 -0700

    Add sleep to TimeService and update FakeTimeService
    Signed-off-by: Zachary Auerbach <zauerbach@pivotal.io>

--- time/fakes/fake_service.go
+++ time/fakes/fake_service.go
@@ -1,13 +1,16 @@
 package fakes
 
-import (
-	"time"
-)
+import "time"
 
 type FakeService struct {
-	NowTime time.Time
+	NowTime       time.Time
+	SleepDuration time.Duration
 }
 
 func (f *FakeService) Now() time.Time {
 	return f.NowTime
+}
+
+func (f *FakeService) Sleep(duration time.Duration) {
+	f.SleepDuration = duration
 }
