7768053ad51f2c6c8cb3d230723d2a93cdc29779
Andrei Dinin <adinin@pivotal.io>
2014-09-15 17:25:32 -0700

    Add DiskUtil to read contents of disk
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

---
+++ platform/deviceutil/diskutil.go
@@ -0,0 +1,58 @@
+package deviceutil
+
+import (
+	"path/filepath"
+
+	boshdisk "github.com/cloudfoundry/bosh-agent/platform/disk"
+	bosherr "github.com/cloudfoundry/bosh-agent/errors"
+	boshsys "github.com/cloudfoundry/bosh-agent/system"
+	boshlog "github.com/cloudfoundry/bosh-agent/logger"
+)
+
+type diskUtil struct {
+	diskPath string
+	mounter boshdisk.Mounter
+	fs      boshsys.FileSystem
+	logger  boshlog.Logger
+	logTag string
+}
+
+func NewDiskUtil(diskPath string, mounter boshdisk.Mounter, fs boshsys.FileSystem, logger boshlog.Logger) DeviceUtil {
+	return diskUtil{
+		diskPath: diskPath,
+		mounter: mounter,
+		fs:       fs,
+		logger: logger,
+		logTag: "diskUtil",
+	}
+}
+
+func (util diskUtil) GetFileContents(fileName string) ([]byte, error) {
+	tempDir, err := util.fs.TempDir("diskutil")
+	if err != nil {
+		return []byte{}, bosherr.WrapError(err, "Creating temporary disk mount point")
+	}
+	defer util.fs.RemoveAll(tempDir)
+
+	err = util.mounter.Mount(util.diskPath, tempDir)
+	if err != nil {
+		return []byte{}, bosherr.WrapError(err, "Mounting disk path %s to %s", util.diskPath, tempDir)
+	}
+	util.logger.Debug(util.logTag, "Mounted disk path %s to %s", util.diskPath, tempDir)
+
+	diskFilePath := filepath.Join(tempDir, fileName)
+	util.logger.Debug(util.logTag, "Reading contents of %s", diskFilePath)
+	contents, err := util.fs.ReadFile(diskFilePath)
+	if err != nil {
+		return []byte{}, bosherr.WrapError(err, "Reading from disk file %s", diskFilePath)
+	}
+	util.logger.Debug(util.logTag, "Got contents of %s: %s", diskFilePath, string(contents))
+
+	_, err = util.mounter.Unmount(util.diskPath)
+	util.logger.Debug(util.logTag, "Unmounting disk path", util.diskPath)
+	if err != nil {
+		return []byte{}, bosherr.WrapError(err, "Unmounting path %s", util.diskPath)
+	}
+
+	return contents, nil
+}
---
+++ platform/deviceutil/diskutil_test.go
@@ -0,0 +1,95 @@
+package deviceutil_test
+
+import (
+	"errors"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	boshlog "github.com/cloudfoundry/bosh-agent/logger"
+	fakesys "github.com/cloudfoundry/bosh-agent/system/fakes"
+	fakedisk "github.com/cloudfoundry/bosh-agent/platform/disk/fakes"
+
+	. "github.com/cloudfoundry/bosh-agent/platform/deviceutil"
+)
+
+var _ = Describe("Diskutil", func() {
+	var (
+		diskUtil DeviceUtil
+		mounter *fakedisk.FakeMounter
+		fs *fakesys.FakeFileSystem
+	)
+
+	BeforeEach(func() {
+		mounter = &fakedisk.FakeMounter{}
+		fs = fakesys.NewFakeFileSystem()
+		logger := boshlog.NewLogger(boshlog.LevelNone)
+		diskUtil = NewDiskUtil("fake-disk-path", mounter, fs, logger)
+	})
+
+	Describe("GetFileContents", func() {
+		BeforeEach(func() {
+			fs.TempDirDir = "fake-tempdir"
+			fs.WriteFileString("fake-tempdir/fake-file-path", "fake-contents")
+		})
+
+		It("mounts disk path to temporary directory", func() {
+			_, err := diskUtil.GetFileContents("fake-file-path")
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(mounter.MountPartitionPaths).To(ContainElement("fake-disk-path"))
+			Expect(mounter.MountMountPoints).To(ContainElement("fake-tempdir"))
+		})
+
+		It("returns contents of file on a disk", func() {
+			contents, err := diskUtil.GetFileContents("fake-file-path")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(string(contents)).To(Equal("fake-contents"))
+		})
+
+		It("unmount disk path", func() {
+			_, err := diskUtil.GetFileContents("fake-file-path")
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(mounter.UnmountPartitionPathOrMountPoint).To(Equal("fake-disk-path"))
+		})
+
+		It("cleans up temporary directory after reading settings", func() {
+			_, err := diskUtil.GetFileContents("fake-file-path")
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(fs.FileExists("fake-tempdir")).To(BeFalse())
+		})
+
+		It("returns error if it fails to create temporary mount directory", func() {
+			fs.TempDirError = errors.New("fake-tempdir-error")
+
+			_, err := diskUtil.GetFileContents("fake-file-path")
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("fake-tempdir-error"))
+		})
+
+		It("returns error if it fails to mount disk path", func() {
+			mounter.MountErr = errors.New("fake-mount-error")
+
+			_, err := diskUtil.GetFileContents("fake-file-path")
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("fake-mount-error"))
+		})
+
+		It("returns an error if it fails to read the file", func() {
+			fs.ReadFileError = errors.New("fake-read-error")
+			_, err := diskUtil.GetFileContents("fake-file-path")
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("fake-read-error"))
+		})
+
+		It("returns error if it fails to unmount disk path", func() {
+			mounter.UnmountErr = errors.New("fake-unmount-error")
+
+			_, err := diskUtil.GetFileContents("fake-file-path")
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("fake-unmount-error"))
+		})
+	})
+})
