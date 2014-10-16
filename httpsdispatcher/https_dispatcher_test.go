2e320e1cb31b2558b0fcaef9e993b9c4c39e3f68
Karl Isenberg <kisenberg@pivotal.io>
2014-10-16 15:11:26 -0700

    Disabled SSLv3 TLS in the micro http server
    - Also disabled support for 3DES & RC4 ciphers
    
    [#80756396]
    
    Signed-off-by: Zachary Auerbach <zak@zaksoup.com>

--- httpsdispatcher/https_dispatcher_test.go
+++ httpsdispatcher/https_dispatcher_test.go
@@ -15,12 +15,13 @@ import (
 
 var _ = Describe("HTTPSDispatcher", func() {
 	var (
-		dispatcher boshdispatcher.HTTPSDispatcher
+		dispatcher *boshdispatcher.HTTPSDispatcher
 	)
 
 	BeforeEach(func() {
-		serverURL, _ := url.Parse("https://127.0.0.1:7788")
 		logger := boshlog.NewLogger(boshlog.LevelNone)
+		serverURL, err := url.Parse("https://127.0.0.1:7788")
+		Expect(err).ToNot(HaveOccurred())
 		dispatcher = boshdispatcher.NewHTTPSDispatcher(serverURL, logger)
 		go dispatcher.Start()
 		time.Sleep(1 * time.Second)
@@ -43,20 +44,149 @@ var _ = Describe("HTTPSDispatcher", func() {
 		client := getHTTPClient()
 		response, err := client.Get("https://127.0.0.1:7788/example")
 
-		Expect(err).To(BeNil())
+		Expect(err).ToNot(HaveOccurred())
 		Expect(response.StatusCode).To(BeNumerically("==", 201))
 		Expect(hasBeenCalled).To(Equal(true))
 	})
 
 	It("returns a 404 if the route does not exist", func() {
 		client := getHTTPClient()
-		response, _ := client.Get("https://127.0.0.1:7788/example")
+		response, err := client.Get("https://127.0.0.1:7788/example")
+		Expect(err).ToNot(HaveOccurred())
 		Expect(response.StatusCode).To(BeNumerically("==", 404))
+	})
+
+	// Go's TLS client does not support SSLv3 (so we couldn't test it even if it did)
+	PIt("does not allow connections using SSLv3", func() {
+		handler := func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) }
+		dispatcher.AddRoute("/example", handler)
+
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true,
+			MinVersion:         tls.VersionSSL30,
+			MaxVersion:         tls.VersionSSL30,
+		}
+		client := getHTTPClientWithConfig(tlsConfig)
+		_, err := client.Get("https://127.0.0.1:7788/example")
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("does allow connections using TLSv1", func() {
+		handler := func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) }
+		dispatcher.AddRoute("/example", handler)
+
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true,
+			MinVersion:         tls.VersionTLS10,
+			MaxVersion:         tls.VersionTLS10,
+		}
+		client := getHTTPClientWithConfig(tlsConfig)
+		_, err := client.Get("https://127.0.0.1:7788/example")
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("does allow connections using TLSv1.1", func() {
+		handler := func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) }
+		dispatcher.AddRoute("/example", handler)
+
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true,
+			MinVersion:         tls.VersionTLS11,
+			MaxVersion:         tls.VersionTLS11,
+		}
+		client := getHTTPClientWithConfig(tlsConfig)
+		_, err := client.Get("https://127.0.0.1:7788/example")
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("does allow connections using TLSv1.2", func() {
+		handler := func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) }
+		dispatcher.AddRoute("/example", handler)
+
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true,
+			MinVersion:         tls.VersionTLS12,
+			MaxVersion:         tls.VersionTLS12,
+		}
+		client := getHTTPClientWithConfig(tlsConfig)
+		_, err := client.Get("https://127.0.0.1:7788/example")
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("does not allow connections using 3DES ciphers", func() {
+		handler := func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) }
+		dispatcher.AddRoute("/example", handler)
+
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true,
+			CipherSuites: []uint16{
+				tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+			},
+		}
+		client := getHTTPClientWithConfig(tlsConfig)
+		_, err := client.Get("https://127.0.0.1:7788/example")
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("does not allow connections using RC4 ciphers", func() {
+		handler := func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) }
+		dispatcher.AddRoute("/example", handler)
+
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true,
+			CipherSuites: []uint16{
+				tls.TLS_RSA_WITH_RC4_128_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+				tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+			},
+		}
+		client := getHTTPClientWithConfig(tlsConfig)
+		_, err := client.Get("https://127.0.0.1:7788/example")
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("does allow connections using AES ciphers", func() {
+		handler := func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) }
+		dispatcher.AddRoute("/example", handler)
+
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true,
+			CipherSuites: []uint16{
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			},
+		}
+		client := getHTTPClientWithConfig(tlsConfig)
+		_, err := client.Get("https://127.0.0.1:7788/example")
+		Expect(err).ToNot(HaveOccurred())
 	})
 })
 
-func getHTTPClient() (httpClient http.Client) {
-	httpTransport := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
-	httpClient = http.Client{Transport: httpTransport}
-	return
+func getHTTPClient() http.Client {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+		// Both CBC & RC4 ciphers can be exploited
+		// Mozilla's "Modern" recommended settings only overlap with the golang TLS client on these two ciphers
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		},
+		// SSLv3 and TLSv1.0 are considered weak
+		// TLS1.1 does not support GCM, so it won't actually be used
+		MinVersion: tls.VersionTLS11,
+		MaxVersion: tls.VersionTLS12,
+	}
+	return getHTTPClientWithConfig(tlsConfig)
+}
+
+func getHTTPClientWithConfig(tlsConfig *tls.Config) http.Client {
+	httpTransport := &http.Transport{TLSClientConfig: tlsConfig}
+	return http.Client{Transport: httpTransport}
 }
