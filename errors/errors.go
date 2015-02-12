17c6459aca02a8f18ce8fe1488782065173dc3cb
Karl Isenberg <kisenberg@pivotal.io>
2015-02-12 14:51:51 -0800

    Rename ComplexError fields to make more sense
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- errors/errors.go
+++ errors/errors.go
@@ -11,30 +11,30 @@ type ShortenableError interface {
 }
 
 type ComplexError struct {
-	Delegate error
-	Cause    error
+	Err   error
+	Cause error
 }
 
 func (e ComplexError) Error() string {
-	return fmt.Sprintf("%s: %s", e.Delegate.Error(), e.Cause.Error())
+	return fmt.Sprintf("%s: %s", e.Err.Error(), e.Cause.Error())
 }
 
 func (e ComplexError) ShortError() string {
-	var delegateMessage string
-	if typedDelegate, ok := e.Delegate.(ShortenableError); ok {
-		delegateMessage = typedDelegate.ShortError()
+	var errorMessage string
+	if shortenableError, ok := e.Err.(ShortenableError); ok {
+		errorMessage = shortenableError.ShortError()
 	} else {
-		delegateMessage = e.Delegate.Error()
+		errorMessage = e.Err.Error()
 	}
 
 	var causeMessage string
-	if typedCause, ok := e.Cause.(ShortenableError); ok {
-		causeMessage = typedCause.ShortError()
+	if shortenableCause, ok := e.Cause.(ShortenableError); ok {
+		causeMessage = shortenableCause.ShortError()
 	} else {
 		causeMessage = e.Cause.Error()
 	}
 
-	return fmt.Sprintf("%s: %s", delegateMessage, causeMessage)
+	return fmt.Sprintf("%s: %s", errorMessage, causeMessage)
 }
 
 func Error(msg string) error {
@@ -53,13 +53,13 @@ func WrapErrorf(cause error, msg string, args ...interface{}) error {
 	return WrapComplexError(cause, Errorf(msg, args...))
 }
 
-func WrapComplexError(cause, delegate error) error {
+func WrapComplexError(cause, err error) error {
 	if cause == nil {
 		cause = Error("<nil cause>")
 	}
 
 	return ComplexError{
-		Delegate: delegate,
-		Cause:    cause,
+		Err:   err,
+		Cause: cause,
 	}
 }
