cloudfoundry/bosh-agent

commit 08d892b708104cd2b794621f3cd095c0b23b7976

Pass a DefaultNetworkResolver to settings.Service instead of a platform
We were injecting a DefaultNetworkResolver into our net managers, which
platform delegated to, which we passed into settings.Service. Now we just pass
that dependency directly.

The DefaultNetworkResolver interface is in the settings package instead of the
net package to avoid an import cycle, which is maybe a little awkward.

[#84893840]

Signed-off-by: Colin OByrne <cobyrne@pivotal.io>

app/app.go
@@ -25,6 +25,8 @@ import (
 	boshmbus "github.com/cloudfoundry/bosh-agent/mbus"
 	boshnotif "github.com/cloudfoundry/bosh-agent/notification"
 	boshplatform "github.com/cloudfoundry/bosh-agent/platform"
+	boshnet "github.com/cloudfoundry/bosh-agent/platform/net"
+	boship "github.com/cloudfoundry/bosh-agent/platform/net/ip"
 	boshsettings "github.com/cloudfoundry/bosh-agent/settings"
 	boshdirs "github.com/cloudfoundry/bosh-agent/settings/directories"
 	boshsyslog "github.com/cloudfoundry/bosh-agent/syslog"
@@ -82,11 +84,15 @@ func (app *app) Setup(args []string) error {
 		app.logger,
 	)
 
+	routesSearcher := boshnet.NewCmdRoutesSearcher(app.platform.GetRunner())
+	ipResolver := boship.NewResolver(boship.NetworkInterfaceToAddrsFunc)
+	defaultNetworkResolver := boshnet.NewDefaultNetworkResolver(routesSearcher, ipResolver)
+
 	settingsService := boshsettings.NewService(
 		app.platform.GetFs(),
 		filepath.Join(dirProvider.BoshDir(), "settings.json"),
 		settingsSource,
-		app.platform,
+		defaultNetworkResolver,
 		app.logger,
 	)
 	boot := boshboot.New(

platform/dummy_platform.go
@@ -1,10 +1,6 @@
 package platform
 
 import (
-	"encoding/json"
-	"path/filepath"
-
-	bosherr "github.com/cloudfoundry/bosh-agent/errors"
 	boshdpresolv "github.com/cloudfoundry/bosh-agent/infrastructure/devicepathresolver"
 	boshlog "github.com/cloudfoundry/bosh-agent/logger"
 	boshcmd "github.com/cloudfoundry/bosh-agent/platform/commands"
@@ -174,21 +170,4 @@ func (p dummyPlatform) GetMonitCredentials() (username, password string, err err
 
 func (p dummyPlatform) PrepareForNetworkingChange() error {
 	return nil
-}
-
-func (p dummyPlatform) GetDefaultNetwork() (boshsettings.Network, error) {
-	var network boshsettings.Network
-
-	networkPath := filepath.Join(p.dirProvider.BoshDir(), "dummy-default-network-settings.json")
-	contents, err := p.fs.ReadFile(networkPath)
-	if err != nil {
-		return network, nil
-	}
-
-	err = json.Unmarshal([]byte(contents), &network)
-	if err != nil {
-		return network, bosherr.WrapError(err, "Unmarshal json settings")
-	}
-
-	return network, nil
 }

platform/fakes/fake_platform.go
@@ -100,10 +100,6 @@ type FakePlatform struct {
 
 	PrepareForNetworkingChangeCalled bool
 	PrepareForNetworkingChangeErr    error
-
-	GetDefaultNetworkCalled  bool
-	GetDefaultNetworkNetwork boshsettings.Network
-	GetDefaultNetworkErr     error
 }
 
 func NewFakePlatform() (platform *FakePlatform) {
@@ -312,9 +308,4 @@ func (p *FakePlatform) GetMonitCredentials() (username, password string, err err
 func (p *FakePlatform) PrepareForNetworkingChange() error {
 	p.PrepareForNetworkingChangeCalled = true
 	return p.PrepareForNetworkingChangeErr
-}
-
-func (p *FakePlatform) GetDefaultNetwork() (boshsettings.Network, error) {
-	p.GetDefaultNetworkCalled = true
-	return p.GetDefaultNetworkNetwork, p.GetDefaultNetworkErr
 }

platform/linux_platform.go
@@ -765,10 +765,6 @@ func (p linux) PrepareForNetworkingChange() error {
 	return nil
 }
 
-func (p linux) GetDefaultNetwork() (boshsettings.Network, error) {
-	return p.netManager.GetDefaultNetwork()
-}
-
 func (p linux) calculateEphemeralDiskPartitionSizes(diskSizeInBytes uint64) (uint64, uint64, error) {
 	memStats, err := p.collector.GetMemStats()
 	if err != nil {

platform/platform_interface.go
@@ -51,7 +51,6 @@ type Platform interface {
 
 	// Network misc
 	PrepareForNetworkingChange() error
-	GetDefaultNetwork() (boshsettings.Network, error)
 
 	// Additional monit management
 	GetMonitCredentials() (username, password string, err error)

platform/provider.go
@@ -64,14 +64,12 @@ func NewProvider(logger boshlog.Logger, dirProvider boshdirs.Provider, options O
 
 	vitalsService := boshvitals.NewService(sigarCollector, dirProvider)
 
-	routesSearcher := boshnet.NewCmdRoutesSearcher(runner)
 	ipResolver := boship.NewResolver(boship.NetworkInterfaceToAddrsFunc)
 
-	defaultNetworkResolver := boshnet.NewDefaultNetworkResolver(routesSearcher, ipResolver)
 	arping := bosharp.NewArping(runner, fs, logger, ArpIterations, ArpIterationDelay, ArpInterfaceCheckDelay)
 
-	centosNetManager := boshnet.NewCentosNetManager(fs, runner, defaultNetworkResolver, ipResolver, arping, logger)
-	ubuntuNetManager := boshnet.NewUbuntuNetManager(fs, runner, defaultNetworkResolver, ipResolver, arping, logger)
+	centosNetManager := boshnet.NewCentosNetManager(fs, runner, ipResolver, arping, logger)
+	ubuntuNetManager := boshnet.NewUbuntuNetManager(fs, runner, ipResolver, arping, logger)
 
 	monitRetryable := NewMonitRetryable(runner)
 	monitRetryStrategy := boshretry.NewAttemptRetryStrategy(10, 1*time.Second, monitRetryable, logger)

platform/dummy_platform_test.go
@@ -1,70 +1,0 @@
-package platform_test
-
-import (
-	"encoding/json"
-	"path/filepath"
-
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
-	boshlog "github.com/cloudfoundry/bosh-agent/logger"
-	. "github.com/cloudfoundry/bosh-agent/platform"
-	fakestats "github.com/cloudfoundry/bosh-agent/platform/stats/fakes"
-	boshsettings "github.com/cloudfoundry/bosh-agent/settings"
-	boshdirs "github.com/cloudfoundry/bosh-agent/settings/directories"
-	fakesys "github.com/cloudfoundry/bosh-agent/system/fakes"
-)
-
-var _ = Describe("dummyPlatform", func() {
-	var (
-		collector   *fakestats.FakeCollector
-		fs          *fakesys.FakeFileSystem
-		cmdRunner   *fakesys.FakeCmdRunner
-		dirProvider boshdirs.Provider
-		platform    Platform
-	)
-
-	BeforeEach(func() {
-		collector = &fakestats.FakeCollector{}
-		fs = fakesys.NewFakeFileSystem()
-		cmdRunner = fakesys.NewFakeCmdRunner()
-		dirProvider = boshdirs.NewProvider("/fake-dir")
-		logger := boshlog.NewLogger(boshlog.LevelNone)
-		platform = NewDummyPlatform(collector, fs, cmdRunner, dirProvider, nil, logger)
-	})
-
-	Describe("GetDefaultNetwork", func() {
-		Context("when default networks settings file is found", func() {
-			expectedNetwork := boshsettings.Network{
-				Default: []string{"fake-default"},
-				DNS:     []string{"fake-dns-name"},
-				IP:      "fake-ip-address",
-				Netmask: "fake-netmask",
-				Gateway: "fake-gateway",
-				Mac:     "fake-mac-address",
-			}
-
-			BeforeEach(func() {
-				settingsPath := filepath.Join(dirProvider.BoshDir(), "dummy-default-network-settings.json")
-
-				expectedNetworkBytes, err := json.Marshal(expectedNetwork)
-				Expect(err).ToNot(HaveOccurred())
-
-				fs.WriteFile(settingsPath, expectedNetworkBytes)
-			})
-
-			It("returns network", func() {
-				network, err := platform.GetDefaultNetwork()
-				Expect(err).ToNot(HaveOccurred())
-				Expect(network).To(Equal(expectedNetwork))
-			})
-		})
-
-		Context("when default networks settings file is not found", func() {
-			It("does not return error because dummy configuration allows no dynamic IP", func() {
-				_, err := platform.GetDefaultNetwork()
-				Expect(err).ToNot(HaveOccurred())
-			})
-		})
-	})
-})

platform/linux_platform_test.go
@@ -1428,25 +1428,6 @@ fake-base-path/data/sys/log/*.log fake-base-path/data/sys/log/*/*.log fake-base-
 		})
 	})
 
-	Describe("GetDefaultNetwork", func() {
-		It("returns default network according to net manager", func() {
-			netManager.GetDefaultNetworkNetwork = boshsettings.Network{IP: "fake-ip"}
-
-			network, err := platform.GetDefaultNetwork()
-			Expect(err).ToNot(HaveOccurred())
-			Expect(network).To(Equal(boshsettings.Network{IP: "fake-ip"}))
-		})
-
-		It("returns error if net manager fails to retrieve default network", func() {
-			netManager.GetDefaultNetworkErr = errors.New("fake-get-default-network-err")
-
-			network, err := platform.GetDefaultNetwork()
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(ContainSubstring("fake-get-default-network-err"))
-			Expect(network).To(Equal(boshsettings.Network{}))
-		})
-	})
-
 	Describe("SetupNetworking", func() {
 		It("delegates to the NetManager", func() {
 			networks := boshsettings.Networks{}

commit 520199ce6012133c22ac9c82d02684ab5f7879a2

Call GetDefaultNetwork through platform so DummyPlatform can fake it
This regressed in 08d892b and causes bosh integration test failures
since the dummy CPI things it's changing the IP while the DummyPlatform
is ignoring the file.

[#84893840]

app/app.go
@@ -25,8 +25,6 @@ import (
 	boshmbus "github.com/cloudfoundry/bosh-agent/mbus"
 	boshnotif "github.com/cloudfoundry/bosh-agent/notification"
 	boshplatform "github.com/cloudfoundry/bosh-agent/platform"
-	boshnet "github.com/cloudfoundry/bosh-agent/platform/net"
-	boship "github.com/cloudfoundry/bosh-agent/platform/net/ip"
 	boshsettings "github.com/cloudfoundry/bosh-agent/settings"
 	boshdirs "github.com/cloudfoundry/bosh-agent/settings/directories"
 	boshsyslog "github.com/cloudfoundry/bosh-agent/syslog"
@@ -76,15 +74,11 @@ func (app *app) Setup(args []string) error {
 		return bosherr.WrapError(err, "Getting Settings Source")
 	}
 
-	routesSearcher := boshnet.NewCmdRoutesSearcher(app.platform.GetRunner())
-	ipResolver := boship.NewResolver(boship.NetworkInterfaceToAddrsFunc)
-	defaultNetworkResolver := boshnet.NewDefaultNetworkResolver(routesSearcher, ipResolver)
-
 	settingsService := boshsettings.NewService(
 		app.platform.GetFs(),
 		filepath.Join(dirProvider.BoshDir(), "settings.json"),
 		settingsSource,
-		defaultNetworkResolver,
+		app.platform,
 		app.logger,
 	)
 	boot := boshboot.New(

platform/dummy_platform.go
@@ -1,6 +1,10 @@
 package platform
 
 import (
+	"encoding/json"
+	"path/filepath"
+
+	bosherr "github.com/cloudfoundry/bosh-agent/errors"
 	boshdpresolv "github.com/cloudfoundry/bosh-agent/infrastructure/devicepathresolver"
 	boshlog "github.com/cloudfoundry/bosh-agent/logger"
 	boshcmd "github.com/cloudfoundry/bosh-agent/platform/commands"
@@ -170,4 +174,21 @@ func (p dummyPlatform) GetMonitCredentials() (username, password string, err err
 
 func (p dummyPlatform) PrepareForNetworkingChange() error {
 	return nil
+}
+
+func (p dummyPlatform) GetDefaultNetwork() (boshsettings.Network, error) {
+	var network boshsettings.Network
+
+	networkPath := filepath.Join(p.dirProvider.BoshDir(), "dummy-default-network-settings.json")
+	contents, err := p.fs.ReadFile(networkPath)
+	if err != nil {
+		return network, nil
+	}
+
+	err = json.Unmarshal([]byte(contents), &network)
+	if err != nil {
+		return network, bosherr.WrapError(err, "Unmarshal json settings")
+	}
+
+	return network, nil
 }

platform/fakes/fake_platform.go
@@ -100,6 +100,9 @@ type FakePlatform struct {
 
 	PrepareForNetworkingChangeCalled bool
 	PrepareForNetworkingChangeErr    error
+
+	GetDefaultNetworkNetwork boshsettings.Network
+	GetDefaultNetworkErr     error
 }
 
 func NewFakePlatform() (platform *FakePlatform) {
@@ -308,4 +311,8 @@ func (p *FakePlatform) GetMonitCredentials() (username, password string, err err
 func (p *FakePlatform) PrepareForNetworkingChange() error {
 	p.PrepareForNetworkingChangeCalled = true
 	return p.PrepareForNetworkingChangeErr
+}
+
+func (p *FakePlatform) GetDefaultNetwork() (boshsettings.Network, error) {
+	return p.GetDefaultNetworkNetwork, p.GetDefaultNetworkErr
 }

platform/linux_platform.go
@@ -65,21 +65,22 @@ type LinuxOptions struct {
 }
 
 type linux struct {
-	fs                 boshsys.FileSystem
-	cmdRunner          boshsys.CmdRunner
-	collector          boshstats.Collector
-	compressor         boshcmd.Compressor
-	copier             boshcmd.Copier
-	dirProvider        boshdirs.Provider
-	vitalsService      boshvitals.Service
-	cdutil             boshdevutil.DeviceUtil
-	diskManager        boshdisk.Manager
-	netManager         boshnet.Manager
-	monitRetryStrategy boshretry.RetryStrategy
-	devicePathResolver boshdpresolv.DevicePathResolver
-	diskScanDuration   time.Duration
-	options            LinuxOptions
-	logger             boshlog.Logger
+	fs                     boshsys.FileSystem
+	cmdRunner              boshsys.CmdRunner
+	collector              boshstats.Collector
+	compressor             boshcmd.Compressor
+	copier                 boshcmd.Copier
+	dirProvider            boshdirs.Provider
+	vitalsService          boshvitals.Service
+	cdutil                 boshdevutil.DeviceUtil
+	diskManager            boshdisk.Manager
+	netManager             boshnet.Manager
+	monitRetryStrategy     boshretry.RetryStrategy
+	devicePathResolver     boshdpresolv.DevicePathResolver
+	diskScanDuration       time.Duration
+	options                LinuxOptions
+	logger                 boshlog.Logger
+	defaultNetworkResolver boshsettings.DefaultNetworkResolver
 }
 
 func NewLinuxPlatform(
@@ -98,23 +99,25 @@ func NewLinuxPlatform(
 	diskScanDuration time.Duration,
 	options LinuxOptions,
 	logger boshlog.Logger,
+	defaultNetworkResolver boshsettings.DefaultNetworkResolver,
 ) Platform {
 	return &linux{
-		fs:                 fs,
-		cmdRunner:          cmdRunner,
-		collector:          collector,
-		compressor:         compressor,
-		copier:             copier,
-		dirProvider:        dirProvider,
-		vitalsService:      vitalsService,
-		cdutil:             cdutil,
-		diskManager:        diskManager,
-		netManager:         netManager,
-		monitRetryStrategy: monitRetryStrategy,
-		devicePathResolver: devicePathResolver,
-		diskScanDuration:   diskScanDuration,
-		options:            options,
-		logger:             logger,
+		fs:                     fs,
+		cmdRunner:              cmdRunner,
+		collector:              collector,
+		compressor:             compressor,
+		copier:                 copier,
+		dirProvider:            dirProvider,
+		vitalsService:          vitalsService,
+		cdutil:                 cdutil,
+		diskManager:            diskManager,
+		netManager:             netManager,
+		monitRetryStrategy:     monitRetryStrategy,
+		devicePathResolver:     devicePathResolver,
+		diskScanDuration:       diskScanDuration,
+		options:                options,
+		logger:                 logger,
+		defaultNetworkResolver: defaultNetworkResolver,
 	}
 }
 
@@ -768,6 +771,10 @@ func (p linux) PrepareForNetworkingChange() error {
 	}
 
 	return nil
+}
+
+func (p linux) GetDefaultNetwork() (boshsettings.Network, error) {
+	return p.defaultNetworkResolver.GetDefaultNetwork()
 }
 
 func (p linux) calculateEphemeralDiskPartitionSizes(diskSizeInBytes uint64) (uint64, uint64, error) {

platform/platform_interface.go
@@ -50,6 +50,7 @@ type Platform interface {
 	GetFilesContentsFromDisk(diskPath string, fileNames []string) (contents [][]byte, err error)
 
 	// Network misc
+	GetDefaultNetwork() (boshsettings.Network, error)
 	PrepareForNetworkingChange() error
 
 	// Additional monit management

platform/provider.go
@@ -72,6 +72,9 @@ func NewProvider(logger boshlog.Logger, dirProvider boshdirs.Provider, options O
 	centosNetManager := boshnet.NewCentosNetManager(fs, runner, ipResolver, interfaceConfigurationCreator, arping, logger)
 	ubuntuNetManager := boshnet.NewUbuntuNetManager(fs, runner, ipResolver, interfaceConfigurationCreator, arping, logger)
 
+	routesSearcher := boshnet.NewCmdRoutesSearcher(runner)
+	linuxDefaultNetworkResolver := boshnet.NewDefaultNetworkResolver(routesSearcher, ipResolver)
+
 	monitRetryable := NewMonitRetryable(runner)
 	monitRetryStrategy := boshretry.NewAttemptRetryStrategy(10, 1*time.Second, monitRetryable, logger)
 
@@ -104,6 +107,7 @@ func NewProvider(logger boshlog.Logger, dirProvider boshdirs.Provider, options O
 		500*time.Millisecond,
 		options.Linux,
 		logger,
+		linuxDefaultNetworkResolver,
 	)
 
 	ubuntu := NewLinuxPlatform(
@@ -122,6 +126,7 @@ func NewProvider(logger boshlog.Logger, dirProvider boshdirs.Provider, options O
 		500*time.Millisecond,
 		options.Linux,
 		logger,
+		linuxDefaultNetworkResolver,
 	)
 
 	return provider{

bootstrap/bootstrap_test.go
@@ -367,6 +367,9 @@ func init() {
 
 				devicePathResolver := devicepathresolver.NewIdentityDevicePathResolver()
 
+				routesSearcher := boshnet.NewCmdRoutesSearcher(runner)
+				defaultNetworkResolver = boshnet.NewDefaultNetworkResolver(routesSearcher, ipResolver)
+
 				platform = boshplatform.NewLinuxPlatform(
 					fs,
 					runner,
@@ -383,10 +386,8 @@ func init() {
 					500*time.Millisecond,
 					linuxOptions,
 					logger,
+					defaultNetworkResolver,
 				)
-
-				routesSearcher := boshnet.NewCmdRoutesSearcher(platform.GetRunner())
-				defaultNetworkResolver = boshnet.NewDefaultNetworkResolver(routesSearcher, ipResolver)
 			})
 
 			JustBeforeEach(func() {
@@ -404,7 +405,7 @@ func init() {
 					platform.GetFs(),
 					settingsPath,
 					settingsSource,
-					defaultNetworkResolver,
+					platform,
 					logger,
 				)
 

platform/dummy_platform_test.go
@@ -1,0 +1,62 @@
+package platform_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	boshdpresolv "github.com/cloudfoundry/bosh-agent/infrastructure/devicepathresolver"
+	fakedpresolv "github.com/cloudfoundry/bosh-agent/infrastructure/devicepathresolver/fakes"
+	boshlog "github.com/cloudfoundry/bosh-agent/logger"
+	. "github.com/cloudfoundry/bosh-agent/platform"
+	boshstats "github.com/cloudfoundry/bosh-agent/platform/stats"
+	fakestats "github.com/cloudfoundry/bosh-agent/platform/stats/fakes"
+	boshdirs "github.com/cloudfoundry/bosh-agent/settings/directories"
+	boshsys "github.com/cloudfoundry/bosh-agent/system"
+	fakesys "github.com/cloudfoundry/bosh-agent/system/fakes"
+)
+
+var _ = Describe("DummyPlatform", describeDummyPlatform)
+
+func describeDummyPlatform() {
+	var (
+		platform           Platform
+		collector          boshstats.Collector
+		fs                 boshsys.FileSystem
+		cmdRunner          boshsys.CmdRunner
+		dirProvider        boshdirs.Provider
+		devicePathResolver boshdpresolv.DevicePathResolver
+		logger             boshlog.Logger
+	)
+
+	BeforeEach(func() {
+		collector = &fakestats.FakeCollector{}
+		fs = fakesys.NewFakeFileSystem()
+		cmdRunner = fakesys.NewFakeCmdRunner()
+		dirProvider = boshdirs.NewProvider("/fake-dir")
+		devicePathResolver = fakedpresolv.NewFakeDevicePathResolver()
+		logger = boshlog.NewLogger(boshlog.LevelNone)
+	})
+
+	JustBeforeEach(func() {
+		platform = NewDummyPlatform(
+			collector,
+			fs,
+			cmdRunner,
+			dirProvider,
+			devicePathResolver,
+			logger,
+		)
+	})
+
+	Describe("GetDefaultNetwork", func() {
+		It("returns the contents of dummy-defaults-network-settings.json since that's what the dummy cpi writes", func() {
+			settingsFilePath := "/fake-dir/bosh/dummy-default-network-settings.json"
+			fs.WriteFileString(settingsFilePath, `{"IP": "1.2.3.4"}`)
+
+			network, err := platform.GetDefaultNetwork()
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(network.IP).To(Equal("1.2.3.4"))
+		})
+	})
+}

platform/linux_platform_test.go
@@ -30,19 +30,20 @@ var _ = Describe("LinuxPlatform", describeLinuxPlatform)
 
 func describeLinuxPlatform() {
 	var (
-		collector          *fakestats.FakeCollector
-		fs                 *fakesys.FakeFileSystem
-		cmdRunner          *fakesys.FakeCmdRunner
-		diskManager        *fakedisk.FakeDiskManager
-		dirProvider        boshdirs.Provider
-		devicePathResolver *fakedpresolv.FakeDevicePathResolver
-		platform           Platform
-		cdutil             *fakedevutil.FakeDeviceUtil
-		compressor         boshcmd.Compressor
-		copier             boshcmd.Copier
-		vitalsService      boshvitals.Service
-		netManager         *fakenet.FakeManager
-		monitRetryStrategy *fakeretry.FakeRetryStrategy
+		collector                  *fakestats.FakeCollector
+		fs                         *fakesys.FakeFileSystem
+		cmdRunner                  *fakesys.FakeCmdRunner
+		diskManager                *fakedisk.FakeDiskManager
+		dirProvider                boshdirs.Provider
+		devicePathResolver         *fakedpresolv.FakeDevicePathResolver
+		platform                   Platform
+		cdutil                     *fakedevutil.FakeDeviceUtil
+		compressor                 boshcmd.Compressor
+		copier                     boshcmd.Copier
+		vitalsService              boshvitals.Service
+		netManager                 *fakenet.FakeManager
+		monitRetryStrategy         *fakeretry.FakeRetryStrategy
+		fakeDefaultNetworkResolver *fakenet.FakeDefaultNetworkResolver
 
 		options LinuxOptions
 		logger  boshlog.Logger
@@ -63,6 +64,7 @@ func describeLinuxPlatform() {
 		netManager = &fakenet.FakeManager{}
 		monitRetryStrategy = fakeretry.NewFakeRetryStrategy()
 		devicePathResolver = fakedpresolv.NewFakeDevicePathResolver()
+		fakeDefaultNetworkResolver = &fakenet.FakeDefaultNetworkResolver{}
 		options = LinuxOptions{}
 
 		fs.SetGlob("/sys/bus/scsi/devices/*:0:0:0/block/*", []string{
@@ -93,6 +95,7 @@ func describeLinuxPlatform() {
 			5*time.Millisecond,
 			options,
 			logger,
+			fakeDefaultNetworkResolver,
 		)
 	})
 
@@ -1448,6 +1451,18 @@ fake-base-path/data/sys/log/*.log fake-base-path/data/sys/log/*/*.log fake-base-
 			Expect(err).ToNot(HaveOccurred())
 
 			Expect(netManager.SetupNetworkingNetworks).To(Equal(networks))
+		})
+	})
+
+	Describe("GetDefaultNetwork", func() {
+		It("delegates to the defaultNetworkResolver", func() {
+			defaultNetwork := boshsettings.Network{IP: "1.2.3.4"}
+			fakeDefaultNetworkResolver.GetDefaultNetworkNetwork = defaultNetwork
+
+			network, err := platform.GetDefaultNetwork()
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(network).To(Equal(defaultNetwork))
 		})
 	})
 }