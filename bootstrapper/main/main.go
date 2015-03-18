7e27d30d691be44d8c3641db8dd8b6fa82b1833b
Jonathan Barnes <jbarnes@pivotal.io>
2015-03-18 11:38:11 -0700

    Add distinguished names as a required parameter
    - fix missing logger.
    
    Signed-off-by: Christian Williams <cwilliams@pivotal.io>

--- bootstrapper/main/main.go
+++ bootstrapper/main/main.go
@@ -2,30 +2,46 @@ package main
 
 import (
 	"fmt"
-	"github.com/cloudfoundry/bosh-agent/bootstrapper"
 	"io/ioutil"
+	"log"
 	"os"
+
+	"github.com/cloudfoundry/bosh-agent/bootstrapper"
 )
 
 func main() {
-	if len(os.Args) != 4 {
-		argv0 := "bootstrapper"
-		fmt.Printf("usage: %s <certFile> <keyFile> <caPEM>\n", argv0)
+	if len(os.Args) != 5 {
+		argv0 := os.Args[0]
+		fmt.Printf("ERROR - Wrong number of arguments\n\n")
+		fmt.Printf("usage: %s <certFile> <keyFile> <caPEM> <allowed distinguished names>\n", argv0)
 		fmt.Println()
-		fmt.Printf("try this: %s bootstrapper/spec/support/certs/bootstrapper.crt bootstrapper/spec/support/certs/bootstrapper.key bootstrapper/spec/support/certs/rootCA.pem\n", argv0)
+		fmt.Printf("try this:\n")
+		fmt.Printf("%s \\\n", argv0)
+		fmt.Printf("   bootstrapper/spec/support/certs/bootstrapper.crt       \\\n")
+		fmt.Printf("   bootstrapper/spec/support/certs/bootstrapper.key       \\\n")
+		fmt.Printf("   bootstrapper/spec/support/certs/rootCA.pem             \\\n")
+		fmt.Printf("   o=bosh.director\n")
 		os.Exit(1)
 	}
 
-	pem, err := ioutil.ReadFile(os.Args[3])
+	certFile := os.Args[1]
+	keyFile := os.Args[2]
+	pemString := os.Args[3]
+	allowedName := os.Args[4]
+
+	pem, err := ioutil.ReadFile(pemString)
 	if err != nil {
 		fmt.Printf("main(): %s\n", err)
 		os.Exit(1)
 	}
 
 	k := &bootstrapper.Bootstrapper{
-		CertFile:  os.Args[1],
-		KeyFile:   os.Args[2],
-		CACertPem: (string)(pem),
+		CertFile:     certFile,
+		KeyFile:      keyFile,
+		CACertPem:    (string)(pem),
+		AllowedNames: []string{allowedName},
+
+		Logger: log.New(os.Stderr, "", log.LstdFlags),
 	}
 
 	err = k.Listen(4443)
