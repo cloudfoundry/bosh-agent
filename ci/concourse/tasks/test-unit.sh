68076bdd6d2c8c012e11b9b815bf5ccf3e1d447d
Corey Innis <cinnis@pivotal.io>
2015-07-29 17:46:03 -0700

    Re-enables a syslog test and forces unit tests runs in concourse to run as vcap, required for the test to pass.
    [#100144184](https://www.pivotaltracker.com/story/show/100144184)

--- ci/concourse/tasks/test-unit.sh
+++ ci/concourse/tasks/test-unit.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
-export GOPATH=$(pwd)/gopath
+# NOTE:
+#   1. bosh-agent unit tests must be run as non-root (syslog suite).
+#   2. at some point, the vcap user may no longer be made available by
+#      garden linux. we will need to add that user in our Dockerfile.
 
-cd gopath/src/github.com/cloudfoundry/bosh-agent
-bin/test-unit
+sudo chown -R vcap:vcap .
+su vcap -c '
+  export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
+  export GOPATH=$(pwd)/gopath
+  cd gopath/src/github.com/cloudfoundry/bosh-agent
+  bin/test-unit
+'
