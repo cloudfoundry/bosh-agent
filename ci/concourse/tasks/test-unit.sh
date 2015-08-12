3306e31f8f7c9f455abb339ca1df673058c7b536
Tyler Schultz <tschultz@pivotal.io>
2015-08-12 16:02:22 -0700

    Removes switching to vcap user for unit test suite.
    No longer required, as we no longer try to listen on a low port in
    tests.
    
    [#100257616](https://www.pivotaltracker.com/story/show/100257616)
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/concourse/tasks/test-unit.sh
+++ ci/concourse/tasks/test-unit.sh
@@ -2,15 +2,7 @@
 
 set -e
 
-# NOTE:
-#   1. bosh-agent unit tests must be run as non-root (syslog suite).
-#   2. at some point, the vcap user may no longer be made available by
-#      garden linux. we will need to add that user in our Dockerfile.
-
-sudo chown -R vcap:vcap .
-su vcap -c '
-  export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
-  export GOPATH=$(pwd)/gopath
-  cd gopath/src/github.com/cloudfoundry/bosh-agent
-  bin/test-unit
-'
+export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
+export GOPATH=$(pwd)/gopath
+cd gopath/src/github.com/cloudfoundry/bosh-agent
+bin/test-unit
