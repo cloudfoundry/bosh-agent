f1169ecd67b61f00d072c3fc1a5c37f11f63ff2b
gu-bin <gubin@cn.ibm.com>
2015-09-07 15:43:39 +0800

    multipath disk resolver

--- platform/provider.go
+++ platform/provider.go
@@ -86,6 +86,10 @@ func NewProvider(logger boshlog.Logger, dirProvider boshdirs.Provider, statsColl
 		devicePathResolver = devicepathresolver.NewVirtioDevicePathResolver(idDevicePathResolver, mappedDevicePathResolver, logger)
 	case "scsi":
 		devicePathResolver = devicepathresolver.NewScsiDevicePathResolver(500*time.Millisecond, fs)
+	// gubin
+	case "multipath":
+		devicePathResolver = devicepathresolver.NewMultipathDevicePathResolver(options.Linux.UsePreformattedPersistentDisk)
+	// ~gubin
 	default:
 		devicePathResolver = devicepathresolver.NewIdentityDevicePathResolver()
 	}
