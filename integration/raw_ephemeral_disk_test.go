3077eaad62d42e5a03a219204f559d09ba446eb5
ergin babani <pair+ebabani@pivotal.io>
2015-09-14 10:33:21 -0400

    Revert "Fixes integration tests by waiting for both devices to show up"
    This reverts commit 2cad84c6d3569bb9ff544596ffcf6a2194502a11.

--- integration/raw_ephemeral_disk_test.go
+++ integration/raw_ephemeral_disk_test.go
@@ -2,6 +2,7 @@ package integration_test
 
 import (
 	"time"
+	"fmt"
 
 	. "github.com/cloudfoundry/bosh-agent/internal/github.com/onsi/ginkgo"
 	. "github.com/cloudfoundry/bosh-agent/internal/github.com/onsi/gomega"
@@ -51,9 +52,11 @@ var _ = Describe("RawEphemeralDisk", func() {
 			err := testEnvironment.AttachDevice("/dev/sdh", 128, 2)
 			Expect(err).ToNot(HaveOccurred())
 
+			fmt.Println("======== attaching devices xvdb")
 			err = testEnvironment.AttachDevice("/dev/xvdb", 128, 1)
 			Expect(err).ToNot(HaveOccurred())
 
+			fmt.Println("======== attaching devices xvdc ")
 			err = testEnvironment.AttachDevice("/dev/xvdc", 128, 1)
 			Expect(err).ToNot(HaveOccurred())
 
@@ -65,6 +68,7 @@ var _ = Describe("RawEphemeralDisk", func() {
 			err = testEnvironment.StartRegistry(registrySettings)
 			Expect(err).ToNot(HaveOccurred())
 
+			fmt.Println("======== start agent")
 			err = testEnvironment.StartAgent()
 			Expect(err).ToNot(HaveOccurred())
 		})
@@ -76,9 +80,11 @@ var _ = Describe("RawEphemeralDisk", func() {
 			err = testEnvironment.DetachDevice("/dev/sdh")
 			Expect(err).ToNot(HaveOccurred())
 
+			fmt.Println("======== ======== detach devices xvdb")
 			err = testEnvironment.DetachDevice("/dev/xvdb")
 			Expect(err).ToNot(HaveOccurred())
 
+			fmt.Println("======== detach devices xvdc")
 			err = testEnvironment.DetachDevice("/dev/xvdc")
 			Expect(err).ToNot(HaveOccurred())
 		})
@@ -87,13 +93,16 @@ var _ = Describe("RawEphemeralDisk", func() {
 			var output string
 
 			Eventually(func() string {
-				stdout, _ := testEnvironment.RunCommand("find /dev/disk/by-partlabel | sort")
+				stdout, _ := testEnvironment.RunCommand("find /dev/disk/by-partlabel")
 
 				output = stdout
 
+//				Expect(err).ToNot(HaveOccurred())
+
 				return stdout
-			}, 2*time.Minute, 1*time.Second).Should(ContainSubstring(`/dev/disk/by-partlabel/raw-ephemeral-0
-/dev/disk/by-partlabel/raw-ephemeral-1`))
+			}, 2*time.Minute, 1*time.Second).Should(ContainSubstring("/dev/disk/by-partlabel/raw-ephemeral-0"))
+
+			Expect(output).To(ContainSubstring("/dev/disk/by-partlabel/raw-ephemeral-1"))
 		})
 	})
 
