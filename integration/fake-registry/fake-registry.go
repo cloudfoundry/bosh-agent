be6346c64fc75b3ca4aa70ccd8dd009ccaba546f
Yulia Gaponenko <yulia.gaponenko@gmail.com>
2015-10-05 09:33:50 -0700

    Vendor bosh-init registry
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- integration/fake-registry/fake-registry.go
+++ integration/fake-registry/fake-registry.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"strings"
 
-	bmregistry "github.com/cloudfoundry/bosh-agent/internal/github.com/cloudfoundry/bosh-init/registry"
+	bmregistry "github.com/cloudfoundry/bosh-init/registry"
 	boshlog "github.com/cloudfoundry/bosh-utils/logger"
 )
 
