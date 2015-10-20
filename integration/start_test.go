e5bad8901314313ecfe79dc21cea1ad0cb7f24dd
Josh Wilks <jwilks@pivotal.io>
2015-10-20 12:29:49 -0400

    Revert "[#104119296] Fix unit and integration tests, and make tests compatible with new 1.5 vendoring"
    This reverts commit e6918192d8e16a6ecec9c383b6388a3f93840468.

---
+++ integration/start_test.go
@@ -0,0 +1,89 @@
+package integration_test
+
+import (
+	"github.com/cloudfoundry/bosh-agent/agentclient"
+	. "github.com/cloudfoundry/bosh-agent/internal/github.com/onsi/ginkgo"
+	. "github.com/cloudfoundry/bosh-agent/internal/github.com/onsi/gomega"
+	boshsettings "github.com/cloudfoundry/bosh-agent/settings"
+)
+
+var _ = Describe("TestStart", func() {
+	var (
+		registrySettings boshsettings.Settings
+		agentClient      agentclient.AgentClient
+	)
+
+	BeforeEach(func() {
+		err := testEnvironment.StopAgent()
+		Expect(err).ToNot(HaveOccurred())
+
+		err = testEnvironment.CleanupDataDir()
+		Expect(err).ToNot(HaveOccurred())
+
+		err = testEnvironment.CleanupLogFile()
+		Expect(err).ToNot(HaveOccurred())
+
+		err = testEnvironment.SetupConfigDrive()
+		Expect(err).ToNot(HaveOccurred())
+
+		err = testEnvironment.UpdateAgentConfig("config-drive-agent.json")
+		Expect(err).ToNot(HaveOccurred())
+
+		registrySettings = boshsettings.Settings{
+			AgentID: "fake-agent-id",
+
+			Mbus: "https://mbus-user:mbus-pass@127.0.0.1:6868",
+
+			Blobstore: boshsettings.Blobstore{
+				Type: "local",
+				Options: map[string]interface{}{
+					"blobstore_path": "/var/vcap/data",
+				},
+			},
+
+			Disks: boshsettings.Disks{
+				Ephemeral: "/dev/sdh",
+			},
+		}
+
+		err = testEnvironment.AttachDevice("/dev/sdh", 128, 2)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = testEnvironment.StartRegistry(registrySettings)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	Context("when start is called", func() {
+		BeforeEach(func() {
+			err := testEnvironment.StartAgent()
+			Expect(err).ToNot(HaveOccurred())
+
+			agentClient, err = testEnvironment.StartAgentTunnel("mbus-user", "mbus-pass", 6868)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			err := testEnvironment.StopAgentTunnel()
+			Expect(err).NotTo(HaveOccurred())
+
+			err = testEnvironment.StopAgent()
+			Expect(err).ToNot(HaveOccurred())
+
+			err = testEnvironment.DetachDevice("/dev/sdh")
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("starts monit service", func() {
+			_, err := testEnvironment.RunCommand("sudo sv stop monit")
+			Expect(err).NotTo(HaveOccurred())
+
+			err = agentClient.Start()
+			Expect(err).NotTo(HaveOccurred())
+
+			result, err := testEnvironment.RunCommand("sudo sv status monit")
+
+			Expect(result).To(ContainSubstring("run: monit: "))
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+})
