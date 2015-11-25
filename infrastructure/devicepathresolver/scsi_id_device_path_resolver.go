6c74b14b02ba8995032bac01e4e1feab336811fb
Yin Ding <yding@vmware.com>
2015-11-25 15:02:42 -0800

    remove ID format check,move logtag into struct

--- infrastructure/devicepathresolver/scsi_id_device_path_resolver.go
+++ infrastructure/devicepathresolver/scsi_id_device_path_resolver.go
@@ -1,7 +1,6 @@
 package devicepathresolver
 
 import (
-	"regexp"
 	"strings"
 	"time"
 
@@ -16,7 +15,9 @@ import (
 type scsiIDDevicePathResolver struct {
 	diskWaitTimeout time.Duration
 	fs              boshsys.FileSystem
-	logger          boshlog.Logger
+
+	logTag	string
+	logger	boshlog.Logger
 }
 
 func NewScsiIDDevicePathResolver(
@@ -27,41 +28,32 @@ func NewScsiIDDevicePathResolver(
 	return scsiIDDevicePathResolver{
 		diskWaitTimeout: diskWaitTimeout,
 		fs:              fs,
-		logger:          logger,
+
+		logTag: "scsiIDresolver",
+		logger: logger,
 	}
 }
-
-const logTag = "scsiIDresolver"
 
 func (idpr scsiIDDevicePathResolver) GetRealDevicePath(diskSettings boshsettings.DiskSettings) (string, bool, error) {
 	if diskSettings.ID == "" {
 		err := bosherr.Errorf("Disk ID is not set")
-		idpr.logger.Error(logTag, err.Error())
-		return "", false, err
-	}
-
-	pattern := `^\w{8}(-\w{4}){3}-\w{12}$`
-	match, _ := regexp.MatchString(pattern, diskSettings.ID)
-
-	if !match {
-		err := bosherr.Errorf("Disk ID is not a UUID")
-		idpr.logger.Error(logTag, err.Error())
+		idpr.logger.Error(idpr.logTag, err.Error())
 		return "", false, err
 	}
 
 	hostPaths, err := idpr.fs.Glob("/sys/class/scsi_host/host*/scan")
 	if err != nil {
 		err := bosherr.WrapError(err, "Could not list SCSI hosts")
-		idpr.logger.Error(logTag, err.Error())
+		idpr.logger.Error(idpr.logTag, err.Error())
 		return "", false, err
 	}
 
 	for _, hostPath := range hostPaths {
-		idpr.logger.Info(logTag, "Performing SCSI rescan of "+hostPath)
+		idpr.logger.Info(idpr.logTag, "Performing SCSI rescan of "+hostPath)
 		err = idpr.fs.WriteFileString(hostPath, "- - -")
 		if err != nil {
 			err := bosherr.WrapError(err, "Starting SCSI rescan")
-			idpr.logger.Error(logTag, err.Error())
+			idpr.logger.Error(idpr.logTag, err.Error())
 			return "", false, err
 		}
 	}
@@ -72,11 +64,11 @@ func (idpr scsiIDDevicePathResolver) GetRealDevicePath(diskSettings boshsettings
 	var realPath string
 
 	for !found {
-		idpr.logger.Debug(logTag, "Waiting for device to appear")
+		idpr.logger.Debug(idpr.logTag, "Waiting for device to appear")
 
 		if time.Now().After(stopAfter) {
 			err := bosherr.Errorf("Timed out getting real device path for '%s'", diskSettings.ID)
-			idpr.logger.Error(logTag, err.Error())
+			idpr.logger.Error(idpr.logTag, err.Error())
 			return "", true, err
 		}
 
@@ -86,18 +78,18 @@ func (idpr scsiIDDevicePathResolver) GetRealDevicePath(diskSettings boshsettings
 		disks, err := idpr.fs.Glob("/dev/disk/by-id/*" + uuid)
 		if err != nil {
 			err := bosherr.WrapError(err, "Could not list disks by id")
-			idpr.logger.Error(logTag, err.Error())
+			idpr.logger.Error(idpr.logTag, err.Error())
 			return "", false, err
 		}
 		for _, path := range disks {
-			idpr.logger.Debug(logTag, "Reading link "+path)
+			idpr.logger.Debug(idpr.logTag, "Reading link "+path)
 			realPath, err = idpr.fs.ReadLink(path)
 			if err != nil {
 				continue
 			}
 
 			if idpr.fs.FileExists(realPath) {
-				idpr.logger.Debug(logTag, "Found real path "+realPath)
+				idpr.logger.Debug(idpr.logTag, "Found real path "+realPath)
 				found = true
 				break
 			}
