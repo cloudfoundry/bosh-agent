872a1ad40c319a2aa1fd61dabd77ff91af3aa9a2
Shatarupa Nandi <snandi@pivotal.io>
2016-01-14 11:51:06 -0800

    Update vendor/bosh-utils to get updates to osFileSystem.FileExists.
    [#111257786](https://www.pivotaltracker.com/story/show/111257786)
    
    Signed-off-by: William Lindner <wlindner@pivotal.io>

--- vendor/github.com/cloudfoundry/bosh-utils/system/fakes/fake_ps_runner.go
+++ vendor/github.com/cloudfoundry/bosh-utils/system/fakes/fake_ps_runner.go
@@ -1,19 +1,35 @@
 package fakes
 
-import boshsys "github.com/cloudfoundry/bosh-utils/system"
+import (
+	"fmt"
+
+	boshsys "github.com/cloudfoundry/bosh-utils/system"
+)
 
 type FakePSRunner struct {
-	RunCommands   []boshsys.PSCommand
-	RunCommandErr error
+	RunCommands      []boshsys.PSCommand
+	RunCommandErr    error
+	runCommandErrors map[string]error
 }
 
 func NewFakePSRunner() *FakePSRunner {
 	return &FakePSRunner{
-		RunCommands: []boshsys.PSCommand{},
+		RunCommands:      []boshsys.PSCommand{},
+		runCommandErrors: map[string]error{},
 	}
 }
 
 func (r *FakePSRunner) RunCommand(cmd boshsys.PSCommand) (string, string, error) {
 	r.RunCommands = append(r.RunCommands, cmd)
+	if err := r.runCommandErrors[cmd.Script]; err != nil {
+		return "", "", err
+	}
 	return "", "", r.RunCommandErr
+}
+
+func (r *FakePSRunner) RegisterRunCommandError(script string, err error) {
+	if _, ok := r.runCommandErrors[script]; ok {
+		panic(fmt.Sprintf("RunCommand error is already set for command: %s", script))
+	}
+	r.runCommandErrors[script] = err
 }
