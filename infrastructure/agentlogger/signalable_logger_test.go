9644107e5595d5a0a8829b51b357e3f4d1a40705
Charlie Vieth <cviethjr@pivotal.io>
2016-02-19 11:04:33 -0500

    Fix regex used for tests and remove null padding from end of printed trace
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- infrastructure/agentlogger/signalable_logger_test.go
+++ infrastructure/agentlogger/signalable_logger_test.go
@@ -2,6 +2,7 @@ package agentlogger_test
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"syscall"
 
@@ -14,8 +15,8 @@ import (
 var _ = Describe("Signalable logger debug", func() {
 	Describe("when SIGSEGV is recieved", func() {
 		It("it dumps all goroutines to stderr", func() {
-			errBuf := bytes.NewBufferString("")
-			outBuf := bytes.NewBufferString("")
+			errBuf := new(bytes.Buffer)
+			outBuf := new(bytes.Buffer)
 			signalChannel := make(chan os.Signal, 1)
 			writerLogger := logger.NewWriterLogger(logger.LevelError, outBuf, errBuf)
 			_, doneChannel := agentlogger.NewSignalableLogger(writerLogger, signalChannel)
@@ -23,8 +24,9 @@ var _ = Describe("Signalable logger debug", func() {
 			signalChannel <- syscall.SIGSEGV
 			<-doneChannel
 
+			fmt.Println(errBuf)
 			Expect(errBuf).To(ContainSubstring("Dumping goroutines"))
-			Expect(errBuf).To(MatchRegexp(`goroutine (\d+) \[syscall\]`))
+			Expect(errBuf).To(MatchRegexp(`goroutine (\d+) \[(syscall|running)\]`))
 		})
 	})
 })
