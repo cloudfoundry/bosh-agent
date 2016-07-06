e403aab95fe865a43ec7b234e7f540a2b9f520ea
Ben Moss <bmoss@pivotal.io>
2016-07-06 17:58:38 -0400

    Add an AMI resource to the Windows agent pipeline

--- ci/tasks/test-integration-windows.sh
+++ ci/tasks/test-integration-windows.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+export AWS_AMI_ID=$(cat windows-ami/version)
 export AGENT_ZIP_URL=$(cat bosh-agent-zip/url)
 export AGENT_DEPS_ZIP_URL=$(cat bosh-agent-deps-zip/url)
 export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
