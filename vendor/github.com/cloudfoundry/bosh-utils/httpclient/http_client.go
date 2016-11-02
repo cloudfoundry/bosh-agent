0923c7eb3d569c2b817b7bd971df89ca7731fe66
Danny Berger <dberger@pivotal.io>
2016-11-02 12:08:35 -0700

    Bump bosh-utils
    [#129998715]
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- vendor/github.com/cloudfoundry/bosh-utils/httpclient/http_client.go
+++ vendor/github.com/cloudfoundry/bosh-utils/httpclient/http_client.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 	"strings"
 
+	"errors"
+	"regexp"
+
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
 	boshlog "github.com/cloudfoundry/bosh-utils/logger"
-	"regexp"
-	"errors"
 )
 
 type HTTPClient interface {
@@ -128,11 +129,11 @@ func (c httpClient) Delete(endpoint string) (*http.Response, error) {
 	return response, nil
 }
 
+var scrubUserinfoRegex = regexp.MustCompile("(https?://.*:).*@")
+
 func scrubErrorOutput(err error) error {
 	errorMsg := err.Error()
-	r := regexp.MustCompile("(http://.*:).*@")
-
-	errorMsg = r.ReplaceAllString(errorMsg, "$1<redacted>@")
+	errorMsg = scrubUserinfoRegex.ReplaceAllString(errorMsg, "$1<redacted>@")
 
 	return errors.New(errorMsg)
 }
