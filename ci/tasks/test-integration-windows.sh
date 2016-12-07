c17ff7c34db59100352c1c742bbccfdc43e2fced
David Jahn <djahn@pivotal.io>
2016-12-07 12:02:52 -0500

    Remove S3 dependencies from Windows integration tests
    * We now compile bosh-agent separately in both unit
    and integration tests.
    * Updated the blobstore-dav and job-service_wrapper binaries, and added
    the the service_wrapper.exe.config to windows/fixtures
    
    [#135606827]
    
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>

--- ci/tasks/test-integration-windows.sh
+++ ci/tasks/test-integration-windows.sh
@@ -3,10 +3,8 @@
 set -ex
 
 export AWS_AMI_ID=$(cat windows-ami/version)
-export AGENT_ZIP_URL=$(cat bosh-agent-zip/url)
-export AGENT_DEPS_ZIP_URL=$(cat bosh-agent-deps-zip/url)
-export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
-export GOPATH=$(pwd)/gopath
+export PATH="/usr/local/ruby/bin:/usr/local/go/bin:$PATH"
+export GOPATH="$(pwd)/gopath"
 
 cd gopath/src/github.com/cloudfoundry/bosh-agent
 bin/test-integration-windows
