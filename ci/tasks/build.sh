a606508067111d9c02b774ec9f49d731c1d6dfda
Tyler Schultz <tschultz@pivotal.io>
2017-03-15 09:44:50 -0700

    Update build script to produce simpler filename
    concourse was unable to parse the version out of the filename
    
    [#134041051](https://www.pivotaltracker.com/story/show/134041051)
    
    Signed-off-by: Mario Manno <mario.manno@gmail.com>

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -6,20 +6,23 @@ export BASE=$(pwd)
 export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
 export GOPATH=${BASE}/gopath
 
-cd gopath/src/github.com/cloudfoundry/bosh-agent
+semver=`cat ${BASE}/version-semver/number`
 
-timestamp=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-git_rev=`git rev-parse --short HEAD`
-go_ver=`go version | cut -d ' ' -f 3`
-version="${timestamp}-${git_rev}-${go_ver}-${GOOS}-${GOARCH}"
-
-filename="bosh-agent-${version}"
-
+filename="bosh-agent-${semver}-${GOOS}-${GOARCH}"
 if [[ $GOOS = 'windows' ]]; then
   filename="${filename}.exe"
 fi
 
+timestamp=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+go_ver=`go version | cut -d ' ' -f 3`
+
+cd gopath/src/github.com/cloudfoundry/bosh-agent
+
+git_rev=`git rev-parse --short HEAD`
+
+version="${semver}-${timestamp}-${git_rev}-${go_ver}-${GOOS}-${GOARCH}"
 sed -i 's/\[DEV BUILD\]/'"$version"'/' main/version.go
+
 bin/build
 
 shasum -a 256 out/bosh-agent
