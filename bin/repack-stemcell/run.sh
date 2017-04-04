46c95f432c7d02816317a155c6457faf551fc80a
Zak Auerbach <zauerbach@pivotal.io>
2017-04-04 11:42:40 -0700

    Rename build-stemcell to repack-stemcell
    fixes
    ```
    hijack: Backend error: Exit status: 500, message: {"Type":"","Message":"runc exec: exit status 1: exec failed: container_linux.go:247: starting container process caused \"exec: \\\"agent-src/bin/build-stemcell/repack.sh\\\": stat agent-src/bin/build-stemcell/repack.sh: no such file or directory\"\n","Handle":""}
    ```
    and
    ```
    error: invalid argument for flag `-c, --config' (expected atc.PathFlag): path './bin/build-stemcell/task.yml' does not exist
    ```
    
    Signed-off-by: Brian Cunnie <bcunnie@pivotal.io>

--- bin/repack-stemcell/run.sh
+++ bin/repack-stemcell/run.sh
@@ -6,5 +6,5 @@ bin=$(dirname $0)
 cd $bin/../..
 ./bin/build-linux-amd64
 mv out/bosh-agent bin/bosh-agent # necessary so that fly -x can be used
-time fly -t production execute -x -p -i agent-src=. -o stemcell=out/ -c ./bin/build-stemcell/task.yml
+time fly -t production execute -x -p -i agent-src=. -o stemcell=out/ -c ./bin/repack-stemcell/task.yml
 ls -la out/stemcell.tgz
