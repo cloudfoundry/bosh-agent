ddb297d562a2b35b9af1dd6db3bec33623a47596
Danny Berger <dberger@pivotal.io>
2017-07-21 14:14:09 -0700

    Switch away from init funcs in agent action tests
    [#148942921](https://www.pivotaltracker.com/story/show/148942921)
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- agent/action/info_test.go
+++ agent/action/info_test.go
@@ -7,28 +7,25 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func init() {
-	Describe("Info", func() {
+var _ = Describe("Info", func() {
+	var (
+		action InfoAction
+	)
 
-		var (
-			action InfoAction
-		)
-
-		BeforeEach(func() {
-			action = NewInfo()
-		})
-
-		AssertActionIsNotAsynchronous(action)
-		AssertActionIsNotPersistent(action)
-		AssertActionIsLoggable(action)
-
-		AssertActionIsNotResumable(action)
-		AssertActionIsNotCancelable(action)
-
-		It("returns the api version", func() {
-			infoResponse, err := action.Run()
-			Expect(err).ToNot(HaveOccurred())
-			Expect(infoResponse.APIVersion).To(Equal(1))
-		})
+	BeforeEach(func() {
+		action = NewInfo()
 	})
-}
+
+	AssertActionIsNotAsynchronous(action)
+	AssertActionIsNotPersistent(action)
+	AssertActionIsLoggable(action)
+
+	AssertActionIsNotResumable(action)
+	AssertActionIsNotCancelable(action)
+
+	It("returns the api version", func() {
+		infoResponse, err := action.Run()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(infoResponse.APIVersion).To(Equal(1))
+	})
+})
--- agent/action/list_disk_test.go
+++ agent/action/list_disk_test.go
@@ -12,35 +12,60 @@ import (
 	boshlog "github.com/cloudfoundry/bosh-utils/logger"
 )
 
-func init() {
-	Describe("ListDisk", func() {
-		var (
-			settingsService *fakesettings.FakeSettingsService
-			platform        *fakeplatform.FakePlatform
-			logger          boshlog.Logger
-			action          ListDiskAction
-		)
+var _ = Describe("ListDisk", func() {
+	var (
+		settingsService *fakesettings.FakeSettingsService
+		platform        *fakeplatform.FakePlatform
+		logger          boshlog.Logger
+		action          ListDiskAction
+	)
 
+	BeforeEach(func() {
+		settingsService = &fakesettings.FakeSettingsService{}
+		platform = fakeplatform.NewFakePlatform()
+		logger = boshlog.NewLogger(boshlog.LevelNone)
+		action = NewListDisk(settingsService, platform, logger)
+	})
+
+	AssertActionIsSynchronousForVersion(action, 1)
+	AssertActionIsSynchronousForVersion(action, 2)
+	AssertActionIsAsynchronousForVersion(action, 3)
+
+	AssertActionIsNotPersistent(action)
+	AssertActionIsLoggable(action)
+
+	AssertActionIsNotResumable(action)
+	AssertActionIsNotCancelable(action)
+
+	It("list disk run", func() {
+		platform.MountedDevicePaths = []string{"/dev/sdb", "/dev/sdc"}
+
+		settingsService.Settings.Disks = boshsettings.Disks{
+			Persistent: map[string]interface{}{
+				"volume-1": "/dev/sda",
+				"volume-2": "/dev/sdb",
+				"volume-3": "/dev/sdc",
+			},
+		}
+
+		value, err := action.Run()
+		Expect(err).ToNot(HaveOccurred())
+		values, ok := value.([]string)
+		Expect(ok).To(BeTrue())
+		Expect(values).To(ContainElement("volume-2"))
+		Expect(values).To(ContainElement("volume-3"))
+		Expect(len(values)).To(Equal(2))
+
+		Expect(settingsService.SettingsWereLoaded).To(BeTrue())
+	})
+
+	Context("when unable to loadsettings", func() {
 		BeforeEach(func() {
-			settingsService = &fakesettings.FakeSettingsService{}
-			platform = fakeplatform.NewFakePlatform()
-			logger = boshlog.NewLogger(boshlog.LevelNone)
-			action = NewListDisk(settingsService, platform, logger)
+			settingsService.LoadSettingsError = bosherrors.Error("fake loadsettings error")
 		})
 
-		AssertActionIsSynchronousForVersion(action, 1)
-		AssertActionIsSynchronousForVersion(action, 2)
-		AssertActionIsAsynchronousForVersion(action, 3)
-
-		AssertActionIsNotPersistent(action)
-		AssertActionIsLoggable(action)
-
-		AssertActionIsNotResumable(action)
-		AssertActionIsNotCancelable(action)
-
-		It("list disk run", func() {
+		It("should return an error", func() {
 			platform.MountedDevicePaths = []string{"/dev/sdb", "/dev/sdc"}
-
 			settingsService.Settings.Disks = boshsettings.Disks{
 				Persistent: map[string]interface{}{
 					"volume-1": "/dev/sda",
@@ -49,37 +74,10 @@ func init() {
 				},
 			}
 
-			value, err := action.Run()
-			Expect(err).ToNot(HaveOccurred())
-			values, ok := value.([]string)
-			Expect(ok).To(BeTrue())
-			Expect(values).To(ContainElement("volume-2"))
-			Expect(values).To(ContainElement("volume-3"))
-			Expect(len(values)).To(Equal(2))
-
-			Expect(settingsService.SettingsWereLoaded).To(BeTrue())
+			_, err := action.Run()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Refreshing the settings: fake loadsettings error"))
 		})
-
-		Context("when unable to loadsettings", func() {
-			BeforeEach(func() {
-				settingsService.LoadSettingsError = bosherrors.Error("fake loadsettings error")
-			})
-
-			It("should return an error", func() {
-				platform.MountedDevicePaths = []string{"/dev/sdb", "/dev/sdc"}
-				settingsService.Settings.Disks = boshsettings.Disks{
-					Persistent: map[string]interface{}{
-						"volume-1": "/dev/sda",
-						"volume-2": "/dev/sdb",
-						"volume-3": "/dev/sdc",
-					},
-				}
-
-				_, err := action.Run()
-				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).To(ContainSubstring("Refreshing the settings: fake loadsettings error"))
-			})
-		})
-
 	})
-}
+
+})
