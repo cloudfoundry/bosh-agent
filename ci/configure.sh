5f1d590f37c70a77e0a7b95c7131e4a044cc4072
Danny Berger <dberger@pivotal.io>
2017-07-28 10:26:03 -0700

    Update pipeline for 2.1.x branch
    [#149319577](https://www.pivotaltracker.com/story/show/149319577)
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 fly -t production set-pipeline \
-    -p bosh-agent \
+    -p bosh-agent:2.1.x \
     -c pipeline.yml \
     --load-vars-from <(lpass show -G "bosh-agent concourse secrets" --notes)
