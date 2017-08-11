cloudfoundry/bosh-agent

commit ae0ba95ae4644fd7d03f8eab0f7323f13316dab7

Windows: refactor service management to be concurrent and not shell out
This commit refactors the Windows Job Supervisor to be perform its
operations concurrently and without shelling out.

The Start and Stop operations were always intended to act on all of the
services at once and without waiting for the requested action to
complete.  Unfortunately we recently discovered this was not the case
and this broke Stop Scripts that waited for all other jobs to stop
before stopping themselves - as the Supervisor would hanging trying to
stop it before advancing on to stop the other services.

The second and larger overvall change is that the Job Supervisor and
associated tests now use the 'golang.org/x/sys/windows/svc' package to
handle service operations instead of shelling out.  Shelling out to
PowerShell is extremely expensive (the service functions start at around
200ms on a fast VM, multiple seconds on a slow VM).  This is one of the
reason that Windows VMs were slow to show up when running `bosh
vms`.  Additionally, there was almost no error handling around the
commands we ran when shelling out - we were only checking if
PowerShell.exe ran correctly - not if any of the scripts threw an error,
which is not reflected in PowerShell's exit code (by default errors are
ignored).

[#148915345](https://www.pivotaltracker.com/story/show/148915345)

jobsupervisor/winsvc/winsvc.go
@@ -1,0 +1,586 @@
+// +build windows
+
+package winsvc
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"syscall"
+	"time"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/svc"
+	"golang.org/x/sys/windows/svc/mgr"
+)
+
+// Mgr is used to manage Windows services.
+type Mgr struct {
+	m     *mgr.Mgr
+	match func(description string) bool // WARN: DEV ONLY
+}
+
+// Connect returns a new Mgr that will monitor all services with descriptions
+// matched by match.  If match is nil all services are matched.
+func Connect(match func(description string) bool) (*Mgr, error) {
+	m, err := mgr.Connect()
+	if err != nil {
+		return nil, err
+	}
+	if match == nil {
+		match = func(_ string) bool { return true }
+	}
+	return &Mgr{m: m, match: match}, nil
+}
+
+// Disconnect closes connection to the service control manager m.
+func (m *Mgr) Disconnect() error {
+	return m.m.Disconnect()
+}
+
+func toString(p *uint16) string {
+	if p == nil {
+		return ""
+	}
+	return syscall.UTF16ToString((*[4096]uint16)(unsafe.Pointer(p))[:])
+}
+
+// serviceDescription, returns the description of service s.
+func (m *Mgr) serviceDescription(s *mgr.Service) (string, error) {
+	var p *windows.SERVICE_DESCRIPTION
+	n := uint32(1024)
+	for {
+		b := make([]byte, n)
+		p = (*windows.SERVICE_DESCRIPTION)(unsafe.Pointer(&b[0]))
+		err := windows.QueryServiceConfig2(s.Handle,
+			windows.SERVICE_CONFIG_DESCRIPTION, &b[0], n, &n)
+		if err == nil {
+			break
+		}
+		if err.(syscall.Errno) != syscall.ERROR_INSUFFICIENT_BUFFER {
+			return "", err
+		}
+		if n <= uint32(len(b)) {
+			return "", err
+		}
+	}
+	return toString(p.Description), nil
+}
+
+// services, returns all of the services that match the Mgr's match function.
+func (m *Mgr) services() ([]*mgr.Service, error) {
+	names, err := m.m.ListServices()
+	if err != nil {
+		return nil, fmt.Errorf("winsvc: listing services: %s", err)
+	}
+	var svcs []*mgr.Service
+	for _, name := range names {
+		s, err := m.m.OpenService(name)
+		if err != nil {
+			continue // ignore - likely access denied
+		}
+		desc, err := m.serviceDescription(s)
+		if err != nil {
+			s.Close()
+			continue // ignore - likely access denied
+		}
+		if m.match(desc) {
+			svcs = append(svcs, s)
+		} else {
+			s.Close()
+		}
+	}
+	return svcs, nil
+}
+
+// iter, calls function fn concurrently on each service matched by Mgr.
+// The service is closed for fn and the first error, if any, is returned.
+//
+// fn must be safe for concurrent use and must not block indefinitely.
+func (m *Mgr) iter(fn func(*mgr.Service) error) (first error) {
+	svcs, err := m.services()
+	if err != nil {
+		return err
+	}
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+	wg.Add(len(svcs))
+	for _, s := range svcs {
+		go func(s *mgr.Service) {
+			defer wg.Done()
+			defer s.Close()
+			if err := fn(s); err != nil {
+				mu.Lock()
+				if first == nil {
+					first = err
+				}
+				mu.Unlock()
+			}
+		}(s)
+	}
+	wg.Wait()
+	return
+}
+
+func svcStartTypeString(startType uint32) string {
+	switch startType {
+	case mgr.StartManual:
+		return "StartManual"
+	case mgr.StartAutomatic:
+		return "StartAutomatic"
+	case mgr.StartDisabled:
+		return "StartDisabled"
+	}
+	return fmt.Sprintf("Invalid Service StartType: %d", startType)
+}
+
+func (m *Mgr) setStartType(s *mgr.Service, startType uint32) error {
+	conf, err := s.Config()
+	if err != nil {
+		return fmt.Errorf("winsvc: querying config for service (%s): %s",
+			s.Name, err)
+	}
+	if conf.StartType == startType {
+		return nil
+	}
+	conf.StartType = startType
+	if err := s.UpdateConfig(conf); err != nil {
+		return fmt.Errorf("winsvc: setting start type of service (%s) to (%s): %s",
+			s.Name, svcStartTypeString(startType), err)
+	}
+	return nil
+}
+
+// querySvc, queries the service status of service s.  This is really here to
+// return a formated error message.
+func querySvc(s *mgr.Service) (svc.Status, error) {
+	status, err := s.Query()
+	if err != nil {
+		err = fmt.Errorf("winsvc: querying status of service (%s): %s", s.Name, err)
+	}
+	return status, err
+}
+
+// calculateWaitHint, converts a service's WaitHint into a time duration and
+// calculates the interval the caller should wait for before rechecking the
+// service's status.
+//
+// If no WaitHint is provided the default of 10 seconds is returned.  As per
+// Microsoft's recommendations he returned interval will be between 1 and 10
+// seconds.
+func calculateWaitHint(status svc.Status) (waitHint, interval time.Duration) {
+	//
+	// This is all a little confusing, so I included the definition of WaitHint
+	// and Microsoft's guidelines on how to use below:
+	//
+	//
+	// Definition of WaitHint:
+	//
+	//   The estimated time required for a pending start, stop, pause, or
+	//   continue operation, in milliseconds. Before the specified amount
+	//   of time has elapsed, the service should make its next call to the
+	//   SetServiceStatus function with either an incremented dwCheckPoint
+	//   value or a change in dwCurrentState. If the amount of time specified
+	//   by dwWaitHint passes, and dwCheckPoint has not been incremented or
+	//   dwCurrentState has not changed, the service control manager or service
+	//   control program can assume that an error has occurred and the service
+	//   should be stopped. However, if the service shares a process with other
+	//   services, the service control manager cannot terminate the service
+	//   application because it would have to terminate the other services
+	//   sharing the process as well.
+	//
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms685996(v=vs.85).aspx
+	//
+	//
+	// Using the wait hint to check for state transition:
+	//
+	//   Do not wait longer than the wait hint. A good interval is
+	//   one-tenth of the wait hint but not less than 1 second
+	//   and not more than 10 seconds.
+	//
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms686315(v=vs.85).aspx
+	//
+	waitHint = time.Duration(status.WaitHint) * time.Millisecond
+	if waitHint == 0 {
+		waitHint = time.Second * 10
+	}
+	interval = waitHint / 10
+	switch {
+	case interval < time.Second:
+		interval = time.Second
+	case interval > time.Second*10:
+		interval = time.Second * 10
+	}
+	return
+}
+
+// A TransitionError is the error returned if a service failed to transition
+// out of a pending state.
+type TransitionError struct {
+	Msg      string
+	Name     string
+	Status   svc.Status
+	WaitHint time.Duration
+	Duration time.Duration
+}
+
+func (e *TransitionError) Error() string {
+	const format = "winsvc: %s: Service %s: State: %s Checkpoint: %d " +
+		"WaitHint: %s TimeElapsed: %s"
+	return fmt.Sprintf(format, e.Msg, e.Name, e.Status.State, e.Status.CheckPoint,
+		e.WaitHint, e.Duration)
+}
+
+// waitPending, waits for service s to transition out of pendingState, which
+// must be either StartPending or StopPending.  A two minute time limit is
+// enforced for the state transition.
+//
+// See calculateWaitHint for an explanation of how the service's WaitHint is
+// used to check progress.
+func waitPending(s *mgr.Service, pendingState svc.State) (svc.Status, error) {
+	// Arbitrary timeout to prevent misbehaving
+	// services from triggering an infinite loop.
+	const Timeout = time.Minute * 2
+
+	if pendingState != svc.StartPending && pendingState != svc.StopPending {
+		// This is a programming error and really should be a panic.
+		return svc.Status{}, fmt.Errorf("winsvc: invalid pending state: %s",
+			svcStateString(pendingState))
+	}
+
+	status, err := querySvc(s)
+	if err != nil {
+		return status, err
+	}
+
+	start := time.Now()
+	checkpoint := start
+	oldCheckpoint := status.CheckPoint
+
+	for i := time.Duration(0); status.State == pendingState; i++ {
+		waitHint, interval := calculateWaitHint(status)
+		time.Sleep(interval) // sleep before rechecking status
+
+		status, err = querySvc(s)
+		if err != nil {
+			return status, err
+		}
+
+		switch {
+		// The service incremented it's checkpoint, reset timer
+		case status.CheckPoint > oldCheckpoint:
+			checkpoint = time.Now()
+			oldCheckpoint = status.CheckPoint
+
+		// No progress made within the wait hint.
+		case time.Since(checkpoint) > waitHint:
+			err := &TransitionError{
+				Msg:      "no progress waiting for state transition",
+				Name:     s.Name,
+				Status:   status,
+				WaitHint: waitHint,
+				Duration: time.Since(start),
+			}
+			return status, err
+
+		// Exceeded our timeout
+		case time.Since(start) > Timeout:
+			err := &TransitionError{
+				Msg:      "timeout waiting for state transition",
+				Name:     s.Name,
+				Status:   status,
+				WaitHint: waitHint,
+				Duration: time.Since(start),
+			}
+			return status, err
+		}
+	}
+
+	if status.State == pendingState {
+		err := &TransitionError{
+			Msg:      "failed to transition out of state",
+			Name:     s.Name,
+			Status:   status,
+			Duration: time.Since(start),
+		}
+		return status, err
+	}
+
+	return status, nil
+}
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms681383(v=vs.85).aspx
+const ERROR_SERVICE_ALREADY_RUNNING = syscall.Errno(0x420)
+
+func (m *Mgr) doStart(s *mgr.Service) error {
+
+	// Set start type to manual to enable starting the service.
+	if err := m.setStartType(s, mgr.StartManual); err != nil {
+		return err
+	}
+
+	status, err := querySvc(s)
+	if err != nil {
+		return err
+	}
+
+	// Wait to transition out of any pending states
+	switch status.State {
+	case svc.StopPending:
+		// If a stop is pending, wait for it
+		status, err = waitPending(s, svc.StopPending)
+		if err != nil {
+			return err
+		}
+	case svc.StartPending:
+		// If a start is pending, wait for it
+		status, err = waitPending(s, svc.StartPending)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Check if the service is already running
+	if status.State == svc.Running {
+		return nil
+	}
+
+	if err := s.Start(); err != nil {
+		// Ignore error if the service is running
+		if err != ERROR_SERVICE_ALREADY_RUNNING {
+			return fmt.Errorf("winsvc: starting service (%s): %s", s.Name, err)
+		}
+	}
+
+	// Wait for the service to start
+	status, err = waitPending(s, svc.StartPending)
+	if err != nil {
+		return err
+	}
+
+	if status.State != svc.Running {
+		return fmt.Errorf("winsvc: start service: service not started. "+
+			"Name: %s State: %s Checkpoint: %d WaitHint: %d", s.Name,
+			svcStateString(status.State), status.CheckPoint, status.WaitHint)
+	}
+	return nil
+}
+
+// Start, starts all of the service monitored by Mgr.
+func (m *Mgr) Start() (first error) {
+	return m.iter(m.doStart)
+}
+
+func (m *Mgr) doStop(s *mgr.Service) error {
+	const Timeout = time.Second * 30
+
+	// Disable service start to prevent flapping services from being
+	// automatically restarted while attempting to stop them.
+	if err := m.setStartType(s, mgr.StartDisabled); err != nil {
+		return err
+	}
+
+	status, err := querySvc(s)
+	if err != nil {
+		return err
+	}
+
+	// Wait to transition out of any pending states
+	switch status.State {
+	case svc.StopPending:
+		// If a stop is pending, wait for it
+		status, err = waitPending(s, svc.StopPending)
+		if err != nil {
+			return err
+		}
+	case svc.StartPending:
+		// If a start is pending, wait for it
+		status, err = waitPending(s, svc.StartPending)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Check if the service is already stopped
+	if status.State == svc.Stopped {
+		return nil
+	}
+
+	// Stop service
+
+	status, err = s.Control(svc.Stop)
+	if err != nil {
+		return fmt.Errorf("winsvc: stopping service (%s): %s", s.Name, err)
+	}
+
+	// Check if the returned status is empty
+	if status.State == 0 || status.Accepts == 0 {
+		status, err = querySvc(s)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Wait for service to stop
+	if status.State == svc.StopPending {
+		status, err = waitPending(s, svc.StopPending)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Check that the service is actually stopped
+	start := time.Now()
+	for status.State != svc.Stopped {
+		_, interval := calculateWaitHint(status)
+		time.Sleep(interval)
+
+		status, err = querySvc(s)
+		if err != nil {
+			return err
+		}
+		if status.State == svc.Stopped {
+			break
+		}
+		if time.Since(start) > Timeout {
+			return fmt.Errorf("winsvc: stop service: timeout waiting for service to stop: %s",
+				s.Name)
+		}
+	}
+
+	return nil
+}
+
+// Stop, stops all of the services monitored by Mgr concurrently and waits
+// for the stop to complete. Stopping services with dependent services is
+// not supported.
+func (m *Mgr) Stop() (first error) {
+	return m.iter(m.doStop)
+}
+
+// Delete, deletes all of the services monitored by Mgr concurrently and waits
+// for the Service Control Manager to remove the services.
+func (m *Mgr) Delete() error {
+	const Timeout = time.Second * 60
+
+	err := m.iter(func(s *mgr.Service) error {
+		if err := s.Delete(); err != nil {
+			return fmt.Errorf("winsvc: deleting service (%s): %s", s.Name, err)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// Wait for services to be deleted - be careful to hold service handles
+	// for the shortest duration possible - as this will prevent the service
+	// from being deleted.
+
+	// Initial sleep interval, start fast and increase by 1s each iteration
+	// to give the SCM time to remove the services.
+	interval := time.Second
+
+	start := time.Now()
+	for {
+		svcs, err := m.services()
+		if err != nil {
+			return err
+		}
+		if len(svcs) == 0 {
+			break
+		}
+		// Immediately close handles
+		for _, s := range svcs {
+			s.Close()
+		}
+		if time.Since(start) > Timeout {
+			return errors.New("winsvc: timeout waiting for services to be deleted")
+		}
+		time.Sleep(interval)
+		if interval < time.Second*10 {
+			interval += time.Second
+		}
+	}
+	return nil
+}
+
+type ServiceStatus struct {
+	Name  string
+	State svc.State
+}
+
+func (s *ServiceStatus) StateString() string {
+	return svcStateString(s.State)
+}
+
+// Status, returns the name and status for all of the services monitored.
+func (m *Mgr) Status() ([]ServiceStatus, error) {
+	svcs, err := m.services()
+	if err != nil {
+		return nil, err
+	}
+	defer closeServices(svcs)
+
+	sts := make([]ServiceStatus, len(svcs))
+	for i, s := range svcs {
+		status, err := querySvc(s)
+		if err != nil {
+			return nil, err
+		}
+		sts[i] = ServiceStatus{Name: s.Name, State: status.State}
+	}
+	return sts, nil
+}
+
+func closeServices(svcs []*mgr.Service) (first error) {
+	for _, s := range svcs {
+		if s == nil || s.Handle == windows.InvalidHandle {
+			continue
+		}
+		if err := s.Close(); err != nil && first == nil {
+			first = err
+		}
+		s.Handle = windows.InvalidHandle
+	}
+	return
+}
+
+// Unmonitor, disable start for all the Mgr m's services.
+func (m *Mgr) Unmonitor() error {
+	return m.iter(func(s *mgr.Service) error {
+		return m.setStartType(s, mgr.StartDisabled)
+	})
+}
+
+// DisableAgentAutoStart, sets the start type of the bosh-agent to disabled.
+func (m *Mgr) DisableAgentAutoStart() error {
+	const name = "bosh-agent"
+	s, err := m.m.OpenService("bosh-agent")
+	if err != nil {
+		return fmt.Errorf("winsvc: opening service (%s): %s", name, err)
+	}
+	defer s.Close()
+	return m.setStartType(s, mgr.StartDisabled)
+}
+
+func svcStateString(s svc.State) string {
+	switch s {
+	case svc.Stopped:
+		return "Stopped"
+	case svc.StartPending:
+		return "StartPending"
+	case svc.StopPending:
+		return "StopPending"
+	case svc.Running:
+		return "Running"
+	case svc.ContinuePending:
+		return "ContinuePending"
+	case svc.PausePending:
+		return "PausePending"
+	case svc.Paused:
+		return "Paused"
+	}
+	return fmt.Sprintf("Invalid Service State: %d", s)
+}

jobsupervisor/windows_job_supervisor.go
@@ -11,16 +11,15 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
 	"sync/atomic"
 	"time"
 
 	"github.com/tedsuo/ifrit"
 	"github.com/tedsuo/ifrit/http_server"
 	"golang.org/x/sys/windows/svc"
-	"golang.org/x/sys/windows/svc/mgr"
 
 	"github.com/cloudfoundry/bosh-agent/jobsupervisor/monitor"
+	"github.com/cloudfoundry/bosh-agent/jobsupervisor/winsvc"
 
 	boshalert "github.com/cloudfoundry/bosh-agent/agent/alert"
 	boshdirs "github.com/cloudfoundry/bosh-agent/settings/directories"
@@ -38,48 +37,15 @@ const (
 	serviceWrapperConfigFileName    = "job-service-wrapper.xml"
 	serviceWrapperAppConfigFileName = "job-service-wrapper.exe.config"
 	serviceWrapperEventJSONFileName = "job-service-wrapper.wrapper.log.json"
-	serviceWrapperAppConfigBody     = `
+
+	serviceWrapperAppConfigBody = `
 <configuration>
   <startup>
     <supportedRuntime version="v4.0" />
   </startup>
 </configuration>
 `
-
-	startJobScript = `
-(get-wmiobject win32_service -filter "description='` + serviceDescription + `'") | ForEach{ Start-Service $_.Name }
-`
-	stopJobScript = `
-(get-wmiobject win32_service -filter "description='` + serviceDescription + `'") | ForEach{ Stop-Service $_.Name }
-`
-	listAllJobsScript = `
-(get-wmiobject win32_service -filter "description='` + serviceDescription + `'") | ForEach{ $_.Name, $_.ProcessId }
-`
-
-	listAllServiceNames = `
-(get-wmiobject win32_service -filter "description='` + serviceDescription + `'") | ForEach{ $_.Name }
-`
-
-	deleteAllJobsScript = `
-(get-wmiobject win32_service -filter "description='` + serviceDescription + `'") | ForEach{ $_.delete() }
-`
-	getStatusScript = `
-(get-wmiobject win32_service -filter "description='` + serviceDescription + `'") | ForEach{ $_.State }
-`
-	unmonitorJobScript = `
-(get-wmiobject win32_service -filter "description='` + serviceDescription + `'") | ForEach{ Set-Service $_.Name -startuptype "Disabled" }
-`
-	manualStartJobScript = `
-(get-wmiobject win32_service -filter "description='` + serviceDescription + `'") | ForEach{ Set-Service $_.Name -startuptype "Manual" }
-`
-
-	waitForDeleteAllScript = `
-(get-wmiobject win32_service -filter "description='` + serviceDescription + `'").Length
-`
-	disableAgentAutoStart = `Set-Service bosh-agent -startuptype "Manual"`
 )
-
-// get-wmiobject win32_service -filter "description='vcap'"
 
 type serviceLogMode struct {
 	Mode          string `xml:"mode,attr"`
@@ -207,6 +173,7 @@ type windowsJobSupervisor struct {
 
 	// state *state.State
 	state supervisorState
+	mgr   *winsvc.Mgr
 }
 
 func (w *windowsJobSupervisor) stateSet(s supervisorState) {
@@ -215,6 +182,10 @@ func (w *windowsJobSupervisor) stateSet(s supervisorState) {
 
 func (w *windowsJobSupervisor) stateIs(s supervisorState) bool {
 	return atomic.LoadInt32((*int32)(&w.state)) == int32(s)
+}
+
+func matchService(description string) bool {
+	return description == serviceDescription
 }
 
 func NewWindowsJobSupervisor(
@@ -243,6 +214,12 @@ func NewWindowsJobSupervisor(
 	}
 	s.monitor = m
 	s.stateSet(stateEnabled)
+
+	mgr, err := winsvc.Connect(matchService)
+	if err != nil {
+		s.logger.Error(s.logTag, "Initializing winsvc.Mgr: %s", err)
+	}
+	s.mgr = mgr
 	return s
 }
 
@@ -257,19 +234,13 @@ func (w *windowsJobSupervisor) Start() error {
 	//
 	// Do this here, as we know the agent has successfully connected
 	// with the director and is healthy.
-	w.cmdRunner.RunCommand("-Command", disableAgentAutoStart)
+	w.mgr.DisableAgentAutoStart()
 
-	_, _, _, err := w.cmdRunner.RunCommand("-Command", manualStartJobScript)
-	if err != nil {
-		return bosherr.WrapError(err, "Starting windows job process")
-	}
-	_, _, _, err = w.cmdRunner.RunCommand("-Command", startJobScript)
-	if err != nil {
+	if err := w.mgr.Start(); err != nil {
 		return bosherr.WrapError(err, "Starting windows job process")
 	}
 
-	err = w.fs.RemoveAll(w.stoppedFilePath())
-	if err != nil {
+	if err := w.fs.RemoveAll(w.stoppedFilePath()); err != nil {
 		return bosherr.WrapError(err, "Removing stopped file")
 	}
 
@@ -278,13 +249,7 @@ func (w *windowsJobSupervisor) Start() error {
 }
 
 func (w *windowsJobSupervisor) Stop() error {
-
-	_, _, _, err := w.cmdRunner.RunCommand("-Command", unmonitorJobScript)
-	if err != nil {
-		return bosherr.WrapError(err, "Disabling services")
-	}
-	_, _, _, err = w.cmdRunner.RunCommand("-Command", stopJobScript)
-	if err != nil {
+	if err := w.mgr.Stop(); err != nil {
 		return bosherr.WrapError(err, "Stopping services")
 	}
 	if err := w.fs.WriteFileString(w.stoppedFilePath(), ""); err != nil {
@@ -294,78 +259,13 @@ func (w *windowsJobSupervisor) Stop() error {
 }
 
 func (w *windowsJobSupervisor) StopAndWait() error {
-	const Timeout = time.Second * 3 // match
-
-	stdout, _, _, err := w.cmdRunner.RunCommand("-Command", listAllServiceNames)
-	if err != nil {
-		return bosherr.WrapError(err, "Disabling services")
-	}
-	names := strings.Split(strings.TrimSpace(stdout), "\r\n")
-	m, err := mgr.Connect()
-	if err != nil {
-		return err // TODO: Wrap
-	}
-	defer m.Disconnect()
-	var svcs []*mgr.Service
-	for _, name := range names {
-		s, err := m.OpenService(name)
-		if err != nil {
-			continue
-		}
-		svcs = append(svcs, s)
-	}
-	defer func() {
-		for _, s := range svcs {
-			s.Close()
-		}
-	}()
-
-	doneCh := make(chan struct{})
-	stopCh := make(chan struct{})
-	defer close(stopCh)
-
-	go func() {
-		tick := time.NewTicker(time.Millisecond * 100)
-		for {
-			select {
-			case <-tick.C:
-				stopped := true
-				for _, s := range svcs {
-					st, err := s.Query()
-					if err != nil || st.State != svc.Stopped {
-						stopped = false
-						break
-					}
-				}
-				if stopped {
-					tick.Stop()
-					close(doneCh)
-					return
-				}
-			case <-stopCh:
-				return
-			}
-		}
-	}()
-
-	err = w.Stop()
-	if err != nil {
-		return err
-	}
-
-	select {
-	case <-doneCh:
-		// Ok
-	case <-time.After(Timeout):
-		return fmt.Errorf("StopAndWait: timed after: %s", Timeout)
-	}
-	return nil
+	// Stop already does this for us
+	return w.Stop()
 }
 
 func (w *windowsJobSupervisor) Unmonitor() error {
 	w.stateSet(stateDisabled)
-	_, _, _, err := w.cmdRunner.RunCommand("-Command", unmonitorJobScript)
-	return err
+	return w.mgr.Unmonitor()
 }
 
 func (w *windowsJobSupervisor) Status() (status string) {
@@ -373,26 +273,19 @@ func (w *windowsJobSupervisor) Status() (status string) {
 		return "stopped"
 	}
 
-	stdout, _, _, err := w.cmdRunner.RunCommand("-Command", getStatusScript)
+	sts, err := w.mgr.Status()
 	if err != nil {
+		fmt.Println("STATUS - ERROR:", err)
 		return "failing"
 	}
-
-	stdout = strings.TrimSpace(stdout)
-	if len(stdout) == 0 {
-		w.logger.Debug(w.logTag, "No statuses reported for job processes")
+	if len(sts) == 0 {
 		return "running"
 	}
-
-	statuses := strings.Split(stdout, "\r\n")
-	w.logger.Debug(w.logTag, "Got statuses %#v", statuses)
-
-	for _, status := range statuses {
-		if status != "Running" {
+	for _, status := range sts {
+		if status.State != svc.Running {
 			return "failing"
 		}
 	}
-
 	return "running"
 }
 
@@ -422,44 +315,14 @@ func (w *windowsJobSupervisor) Processes() ([]Process, error) {
 	// logging very heavily are what will be responsible for
 	// the majority of system usage.
 
-	stdout, _, _, err := w.cmdRunner.RunCommand("-Command", listAllJobsScript)
+	sts, err := w.mgr.Status()
 	if err != nil {
-		return nil, bosherr.WrapError(err, "Listing windows job process")
+		return nil, bosherr.WrapError(err, "Getting windows job process status")
 	}
-
-	m, err := mgr.Connect()
-	if err != nil {
-		return nil, err
+	procs := make([]Process, len(sts))
+	for i, st := range sts {
+		procs[i] = Process{Name: st.Name, State: SvcStateString(st.State)}
 	}
-	defer m.Disconnect()
-
-	var procs []Process
-	lines := strings.Split(stdout, "\n")
-
-	for i := 0; i < len(lines)-1; i += 2 {
-		name := strings.TrimSpace(lines[i])
-		if name == "" {
-			continue
-		}
-		_ = lines[i+1] // pid string
-
-		service, err := m.OpenService(name)
-		if err != nil {
-			return nil, bosherr.WrapErrorf(err, "Opening windows service: %q", name)
-		}
-		defer service.Close()
-		st, err := service.Query()
-		if err != nil {
-			return nil, bosherr.WrapErrorf(err, "Querying windows service: %q", name)
-		}
-
-		p := Process{
-			Name:  name,
-			State: SvcStateString(st.State),
-		}
-		procs = append(procs, p)
-	}
-
 	return procs, nil
 }
 
@@ -533,41 +396,7 @@ func (w *windowsJobSupervisor) AddJob(jobName string, jobIndex int, configPath s
 }
 
 func (w *windowsJobSupervisor) RemoveAllJobs() error {
-
-	const MaxRetries = 100
-	const RetryInterval = time.Millisecond * 5
-
-	_, _, _, err := w.cmdRunner.RunCommand("-Command", deleteAllJobsScript)
-	if err != nil {
-		return bosherr.WrapErrorf(err, "Removing Windows job supervisor services")
-	}
-
-	i := 0
-	start := time.Now()
-	for {
-		stdout, _, _, err := w.cmdRunner.RunCommand("-Command", waitForDeleteAllScript)
-		if err != nil {
-			return bosherr.WrapErrorf(err, "Checking if Windows job supervisor services exist")
-		}
-		if strings.TrimSpace(stdout) == "0" {
-			break
-		}
-
-		i++
-		if i == MaxRetries {
-			return bosherr.Errorf("removing Windows job supervisor services after %d attempts",
-				MaxRetries)
-		}
-		w.logger.Debug(w.logTag, "Waiting for services to be deleted: attempt (%d) time (%s)",
-			i, time.Since(start))
-
-		time.Sleep(RetryInterval)
-	}
-
-	w.logger.Debug(w.logTag, "Removed Windows job supervisor services: attempts (%d) time (%s)",
-		i, time.Since(start))
-
-	return nil
+	return w.mgr.Delete()
 }
 
 type windowsServiceEvent struct {

commit 412cb7d9ece39fb7cb70db18a216aabac95ad0dd

Windows: set Agent start type Manual instead of Disabled
Fixes a bug introduced by
[ae0ba95](https://github.com/cloudfoundry/bosh-agent/commit/ae0ba95ae4644fd7d03f8eab0f7323f13316dab7)
that set the Agent's startup type to Disabled instead of Manual.

jobsupervisor/winsvc/winsvc.go
@@ -19,7 +19,7 @@ import (
 type Mgr struct {
 	m     *mgr.Mgr
 	mon   *monitor
-	match func(description string) bool // WARN: DEV ONLY
+	match func(description string) bool
 }
 
 // Connect returns a new Mgr that will monitor all services with descriptions
@@ -565,7 +565,7 @@ func (m *Mgr) Unmonitor() error {
 	})
 }
 
-// DisableAgentAutoStart sets the start type of the bosh-agent to disabled.
+// DisableAgentAutoStart sets the start type of the bosh-agent to manual.
 func (m *Mgr) DisableAgentAutoStart() error {
 	const name = "bosh-agent"
 	s, err := m.m.OpenService("bosh-agent")
@@ -573,7 +573,7 @@ func (m *Mgr) DisableAgentAutoStart() error {
 		return &ServiceError{"opening service", name, err}
 	}
 	defer s.Close()
-	return m.setStartType(s, mgr.StartDisabled)
+	return m.setStartType(s, mgr.StartManual)
 }
 
 func svcStateString(s svc.State) string {