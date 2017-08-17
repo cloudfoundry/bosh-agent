d9b9d6c2ef8e09ac65f61d7ebda14cdb9c3c847e
Charlie Vieth <cviethjr@pivotal.io>
2017-08-17 15:17:40 -0400

    Windows Integration: compress Windows fixtures
    WinRM/Vagrant are struggling to send our fixtures to the Windows VM used
    for the integration tests.  This commit compresses them into a .tgz
    before sending reducing the total file size from ~28MB to 10MB.
    
    [#150233685](https://www.pivotaltracker.com/story/show/150233685)
    
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>

--- integration/windows/windows_suite_test.go
+++ integration/windows/windows_suite_test.go
@@ -1,7 +1,10 @@
 package windows_test
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/cloudfoundry/bosh-agent/integration/windows/utils"
@@ -11,18 +14,75 @@ import (
 	"testing"
 )
 
+var VagrantProvider = os.Getenv("VAGRANT_PROVIDER")
+
 func TestWindows(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Windows Suite")
 }
 
-var vagrantProvider = os.Getenv("VAGRANT_PROVIDER")
+func tarFixtures(filename string) error {
+	fixtures := []string{
+		"fixtures/service_wrapper.xml",
+		"fixtures/service_wrapper.exe",
+		"fixtures/job-service-wrapper.exe",
+		"fixtures/bosh-blobstore-dav.exe",
+		"fixtures/bosh-agent.exe",
+		"fixtures/pipe.exe",
+	}
+
+	archive, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer archive.Close()
+
+	gzipWriter := gzip.NewWriter(archive)
+	tarWriter := tar.NewWriter(gzipWriter)
+
+	for _, name := range fixtures {
+		fi, err := os.Stat(name)
+		if err != nil {
+			return err
+		}
+
+		hdr, err := tar.FileInfoHeader(fi, "")
+		if err != nil {
+			return err
+		}
+
+		if err := tarWriter.WriteHeader(hdr); err != nil {
+			return err
+		}
+
+		f, err := os.Open(name)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		if _, err := io.Copy(tarWriter, f); err != nil {
+			return err
+		}
+	}
+
+	if err := tarWriter.Close(); err != nil {
+		return err
+	}
+	if err := gzipWriter.Close(); err != nil {
+		return err
+	}
+	return nil
+}
 
 var _ = BeforeSuite(func() {
 	if _, ok := os.LookupEnv("NATS_PRIVATE_IP"); !ok {
-		Fail("Environment variable NATS_PRIVATE_IP not set", 1)
+		Fail("Environment variable NATS_PRIVATE_IP not set (default is 172.31.180.3 if running locally)", 1)
 	}
-	_, err := utils.StartVagrant(vagrantProvider)
+	if err := tarFixtures("fixtures/fixtures.tgz"); err != nil {
+		Fail(fmt.Sprintln("Creating fixtures TGZ::", err))
+	}
+	_, err := utils.StartVagrant(VagrantProvider)
 	if err != nil {
 		Fail(fmt.Sprintln("Could not build the bosh-agent project.\nError is:", err))
 	}
