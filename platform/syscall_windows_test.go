343749af6537ac9f5433d2eb189c529b66a98183
Julien Cherry <jcherry@pivotal.io>
2017-11-07 11:02:05 -0500

    added tests and updated disable winrm implementation
    [#151329914]
    
    Signed-off-by: Lisa Oakley <loakley@pivotal.io>

--- platform/syscall_windows_test.go
+++ platform/syscall_windows_test.go
@@ -2,6 +2,13 @@
 
 package platform
 
+import (
+	"os/exec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
 var (
 	// Export for testing
 	UserHomeDirectory    = userHomeDirectory
@@ -26,3 +33,54 @@ func SetAdministratorUserName(name string) (previous string) {
 	administratorUserName = name
 	return previous
 }
+
+var _ = FDescribe("closeWinRMPort", func() {
+
+	var itAddsABlockingRule = func() {
+		err := closeWinRMPort()
+		Expect(err).ToNot(HaveOccurred())
+
+		cmd := exec.Command("Powershell", "-Command", "Get-NetFirewallRule | where { $_.Action -eq \"Block\" } | Get-NetFirewallPortFilter | where { $_.LocalPort -eq 5985 }")
+		out, err := cmd.CombinedOutput()
+		s := string(out)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(s).ToNot(BeEmpty())
+	}
+	cmd := exec.Command("Powershell", "-Command", "Get-NetFirewallRule | where { $_.Action -eq \"Allow\" } | Get-NetFirewallPortFilter | where { $_.LocalPort -eq 5985 }")
+	Context("Firewall rule allowing port 5985 exists", func() {
+
+		BeforeEach(func() {
+			deleteAllWinRMFirewallRules()
+
+			err := setWinrmFirewall("allow")
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("closes the port", func() {
+			err := closeWinRMPort()
+			Expect(err).ToNot(HaveOccurred())
+
+			out, err := cmd.CombinedOutput()
+			s := string(out)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(s).To(BeEmpty())
+		})
+
+		It("adds a blocking rule", itAddsABlockingRule)
+	})
+
+	Context("Firewall rule allowing port 5985 does NOT exist", func() {
+		BeforeEach(func() {
+			deleteAllWinRMFirewallRules()
+		})
+
+		It("does not error", func() {
+			err := closeWinRMPort()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("adds a blocking rule", itAddsABlockingRule)
+	})
+})
