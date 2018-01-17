c5be710f309acf49de216cc26e1b0671d6992edb
Bozhidar Lenchov <blenchov@pivotal.io>
2018-01-17 17:38:09 -0500

    As an agent api client, i expect to be able to mount_disk that was just attached to the vm at the iaas level
    - Agent accepts disk hints as 2nd argument to mount_disk (backwards compatible)
    
    [#151693862](https://www.pivotaltracker.com/story/show/151693862)
    
    Signed-off-by: Jamil Shamy <jshamy@pivotal.io>

--- settings/settings.go
+++ settings/settings.go
@@ -87,41 +87,17 @@ type VM struct {
 }
 
 func (s Settings) PersistentDiskSettings(diskID string) (DiskSettings, bool) {
-	diskSettings := DiskSettings{}
-
 	for key, settings := range s.Disks.Persistent {
 		if key == diskID {
-			diskSettings.ID = diskID
-
-			if hashSettings, ok := settings.(map[string]interface{}); ok {
-				if path, ok := hashSettings["path"]; ok {
-					diskSettings.Path = path.(string)
-				}
-				if volumeID, ok := hashSettings["volume_id"]; ok {
-					diskSettings.VolumeID = volumeID.(string)
-				}
-				if deviceID, ok := hashSettings["id"]; ok {
-					diskSettings.DeviceID = deviceID.(string)
-				}
-				if lun, ok := hashSettings["lun"]; ok {
-					diskSettings.Lun = lun.(string)
-				}
-				if hostDeviceID, ok := hashSettings["host_device_id"]; ok {
-					diskSettings.HostDeviceID = hostDeviceID.(string)
-				}
-			} else {
-				// Old CPIs return disk path (string) or volume id (string) as disk settings
-				diskSettings.Path = settings.(string)
-				diskSettings.VolumeID = settings.(string)
-			}
-
-			diskSettings.FileSystemType = s.Env.PersistentDiskFS
-			diskSettings.MountOptions = s.Env.PersistentDiskMountOptions
-			return diskSettings, true
+			return s.populatePersistentDiskSettings(diskID, settings), true
 		}
 	}
 
-	return diskSettings, false
+	return DiskSettings{}, false
+}
+
+func (s Settings) PersistentDiskSettingsFromHint(diskID string, diskHint interface{}) DiskSettings {
+	return s.populatePersistentDiskSettings(diskID, diskHint)
 }
 
 func (s Settings) EphemeralDiskSettings() DiskSettings {
@@ -178,6 +154,39 @@ func (s Settings) GetNtpServers() []string {
 		return s.Env.Bosh.NTP
 	}
 	return s.NTP
+}
+
+func (s Settings) populatePersistentDiskSettings(diskID string, settingsInfo interface{}) DiskSettings {
+	diskSettings := DiskSettings{
+		ID: diskID,
+	}
+
+	if hashSettings, ok := settingsInfo.(map[string]interface{}); ok {
+		if path, ok := hashSettings["path"]; ok {
+			diskSettings.Path = path.(string)
+		}
+		if volumeID, ok := hashSettings["volume_id"]; ok {
+			diskSettings.VolumeID = volumeID.(string)
+		}
+		if deviceID, ok := hashSettings["id"]; ok {
+			diskSettings.DeviceID = deviceID.(string)
+		}
+		if lun, ok := hashSettings["lun"]; ok {
+			diskSettings.Lun = lun.(string)
+		}
+		if hostDeviceID, ok := hashSettings["host_device_id"]; ok {
+			diskSettings.HostDeviceID = hostDeviceID.(string)
+		}
+	} else if stringSetting, ok := settingsInfo.(string); ok {
+		// Old CPIs return disk path (string) or volume id (string) as disk settings
+		diskSettings.Path = stringSetting
+		diskSettings.VolumeID = stringSetting
+	}
+
+	diskSettings.FileSystemType = s.Env.PersistentDiskFS
+	diskSettings.MountOptions = s.Env.PersistentDiskMountOptions
+
+	return diskSettings
 }
 
 type Env struct {
