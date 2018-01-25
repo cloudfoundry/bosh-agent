cf72d747c2250e33d76c4b40f3fabf92ae7d08d8
Difan Zhao <dzhao@pivotal.io>
2018-01-25 10:29:57 -0800

    cut new pipeline for 2.57.x
    [#154536765](https://www.pivotaltracker.com/story/show/154536765)
    
    Signed-off-by: J Evelyn Quintana <jquintana@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 fly -t production set-pipeline \
-    -p bosh-agent \
+    -p bosh-agent:2.57.x \
     -c pipeline.yml \
-    --load-vars-from <(lpass show -G "bosh-agent concourse secrets" --notes)
+    --load-vars-from <(lpass show -G "bosh-agent concourse secrets" --notes) \
+    --var agent_branch=2.57.x \
+    --var version_bump='patch' \
+    --var agent_version_key='agent-2.57-current-version' \
+    --var agent_initial_version=2.57.0
