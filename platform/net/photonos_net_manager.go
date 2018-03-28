0cfe71b00a7740268dedb215fa794953445719bc
DheerajSShetty <dheerajs@vmware.com>
2018-03-27 19:38:27 -0700

    Add PhotonOS support

---
+++ platform/net/photonos_net_manager.go
@@ -0,0 +1,230 @@
+package net
+
+import (
+	bosharp "github.com/cloudfoundry/bosh-agent/platform/net/arp"
+	boship "github.com/cloudfoundry/bosh-agent/platform/net/ip"
+	boshsettings "github.com/cloudfoundry/bosh-agent/settings"
+	bosherr "github.com/cloudfoundry/bosh-utils/errors"
+	boshlog "github.com/cloudfoundry/bosh-utils/logger"
+	boshsys "github.com/cloudfoundry/bosh-utils/system"
+	ipnet "net"
+	"strconv"
+	"strings"
+)
+
+const photonosNetManagerLogTag = "photonosNetManager"
+
+type photonosNetManager struct {
+	fs                            boshsys.FileSystem
+	cmdRunner                     boshsys.CmdRunner
+	routesSearcher                RoutesSearcher
+	ipResolver                    boship.Resolver
+	interfaceConfigurationCreator InterfaceConfigurationCreator
+	interfaceAddressesValidator   boship.InterfaceAddressesValidator
+	dnsValidator                  DNSValidator
+	addressBroadcaster            bosharp.AddressBroadcaster
+	logger                        boshlog.Logger
+}
+
+func NewPhotonosNetManager(
+	fs boshsys.FileSystem,
+	cmdRunner boshsys.CmdRunner,
+	ipResolver boship.Resolver,
+	interfaceConfigurationCreator InterfaceConfigurationCreator,
+	interfaceAddressesValidator boship.InterfaceAddressesValidator,
+	dnsValidator DNSValidator,
+	addressBroadcaster bosharp.AddressBroadcaster,
+	logger boshlog.Logger,
+) Manager {
+	return photonosNetManager{
+		fs:                            fs,
+		cmdRunner:                     cmdRunner,
+		ipResolver:                    ipResolver,
+		interfaceConfigurationCreator: interfaceConfigurationCreator,
+		interfaceAddressesValidator:   interfaceAddressesValidator,
+		dnsValidator:                  dnsValidator,
+		addressBroadcaster:            addressBroadcaster,
+		logger:                        logger,
+	}
+}
+
+func (net photonosNetManager) SetupIPv6(_ boshsettings.IPv6, _ <-chan struct{}) error { return nil }
+
+func (net photonosNetManager) SetupNetworking(networks boshsettings.Networks, errCh chan error) error {
+	nonVipNetworks := boshsettings.Networks{}
+	for networkName, networkSettings := range networks {
+		if networkSettings.IsVIP() {
+			continue
+		}
+		nonVipNetworks[networkName] = networkSettings
+	}
+
+	staticInterfaceConfigurations, dhcpInterfaceConfigurations, err := net.buildInterfaces(nonVipNetworks)
+	if err != nil {
+		return err
+	}
+
+	dnsNetwork, _ := nonVipNetworks.DefaultNetworkFor("dns")
+	dnsServers := dnsNetwork.DNS
+
+	err = net.writeNetworkInterfaces(dhcpInterfaceConfigurations, staticInterfaceConfigurations, dnsServers)
+	if err != nil {
+		return bosherr.WrapError(err, "Writing network configuration")
+	}
+
+	net.restartNetworkingInterfaces()
+
+	staticAddresses, dynamicAddresses := net.ifaceAddresses(staticInterfaceConfigurations, dhcpInterfaceConfigurations)
+
+	err = net.interfaceAddressesValidator.Validate(staticAddresses)
+	if err != nil {
+		return bosherr.WrapError(err, "Validating static network configuration")
+	}
+
+	err = net.dnsValidator.Validate(dnsServers)
+	if err != nil {
+		return bosherr.WrapError(err, "Validating dns configuration")
+	}
+
+	net.broadcastIps(append(staticAddresses, dynamicAddresses...), errCh)
+
+	return nil
+}
+
+func (net photonosNetManager) GetConfiguredNetworkInterfaces() ([]string, error) {
+	interfaces := []string{}
+
+	interfacesByMacAddress, err := net.detectMacAddresses()
+	if err != nil {
+		return interfaces, bosherr.WrapError(err, "Getting network interfaces")
+	}
+
+	for _, iface := range interfacesByMacAddress {
+		interfaces = append(interfaces, iface)
+	}
+
+	return interfaces, nil
+}
+
+func (net photonosNetManager) maskToCIDR(netMask string) int {
+	maskSize := ipnet.IPMask(ipnet.ParseIP(netMask).To4())
+	cidr, _ := maskSize.Size()
+	return cidr
+}
+
+func (net photonosNetManager) writeNetworkInterfaces(dhcpInterfaceConfigurations []DHCPInterfaceConfiguration, staticInterfaceConfigurations []StaticInterfaceConfiguration, dnsServers []string) error {
+
+	for _, interfaceConfigInfo := range staticInterfaceConfigurations {
+
+		_, _, _, err := net.cmdRunner.RunCommand("netmgr", "ip4_address", "--set", "--interface", interfaceConfigInfo.Name,
+			"--mode", "static", "--addr", interfaceConfigInfo.Address+"/"+strconv.Itoa(net.maskToCIDR(interfaceConfigInfo.Netmask)),
+			"--gateway", interfaceConfigInfo.Gateway)
+		if err != nil {
+			return bosherr.WrapErrorf(err, "Setting Address '%s' and Gateway '%s' failed", interfaceConfigInfo.Name, interfaceConfigInfo.Gateway)
+		}
+
+		_, _, _, err = net.cmdRunner.RunCommand("netmgr", "link_info", "--set", "--interface", interfaceConfigInfo.Name,
+			"--macaddr", interfaceConfigInfo.Mac)
+		if err != nil {
+			return bosherr.WrapError(err, "Setting Mac Address failed")
+		}
+	}
+
+	for _, dhcpConfig := range dhcpInterfaceConfigurations {
+
+		_, _, _, err := net.cmdRunner.RunCommand("netmgr", "ip4_address", "--set", "--interface", dhcpConfig.Name,
+			"--mode", "dhcp")
+		if err != nil {
+			return bosherr.WrapErrorf(err, "Setting DHCP as IP option for the interface '%s' failed", dhcpConfig.Name)
+		}
+
+	}
+
+	for i, dnsServer := range dnsServers {
+		if i == 0 {
+			_, _, _, err := net.cmdRunner.RunCommand("netmgr", "dns_servers", "--set", "--mode", "static",
+				"--servers", dnsServer)
+			if err != nil {
+				return bosherr.WrapErrorf(err, "Setting DNS Server failed")
+			}
+		} else {
+			_, _, _, err := net.cmdRunner.RunCommand("netmgr", "dns_servers", "--add", "--servers", dnsServer)
+
+			if err != nil {
+				return bosherr.WrapErrorf(err, "Setting DNS Server failed")
+			}
+		}
+	}
+	return nil
+
+}
+
+func (net photonosNetManager) buildInterfaces(networks boshsettings.Networks) ([]StaticInterfaceConfiguration, []DHCPInterfaceConfiguration, error) {
+	interfacesByMacAddress, err := net.detectMacAddresses()
+	if err != nil {
+		return nil, nil, bosherr.WrapError(err, "Getting network interfaces")
+	}
+
+	staticInterfaceConfigurations, dhcpInterfaceConfigurations, err := net.interfaceConfigurationCreator.CreateInterfaceConfigurations(networks, interfacesByMacAddress)
+
+	if err != nil {
+		return nil, nil, bosherr.WrapError(err, "Creating interface configurations")
+	}
+
+	return staticInterfaceConfigurations, dhcpInterfaceConfigurations, nil
+}
+
+func (net photonosNetManager) broadcastIps(addresses []boship.InterfaceAddress, errCh chan error) {
+	go func() {
+		net.addressBroadcaster.BroadcastMACAddresses(addresses)
+		if errCh != nil {
+			errCh <- nil
+		}
+	}()
+}
+
+func (net photonosNetManager) restartNetworkingInterfaces() {
+	net.logger.Debug(photonosNetManagerLogTag, "Restarting network interfaces")
+
+	_, _, _, err := net.cmdRunner.RunCommand("systemctl", "restart", "systemd-networkd")
+	if err != nil {
+		net.logger.Error(photonosNetManagerLogTag, "Ignoring network restart failure: %s", err.Error())
+	}
+}
+
+func (net photonosNetManager) detectMacAddresses() (map[string]string, error) {
+	addresses := map[string]string{}
+
+	stdout, _, _, err := net.cmdRunner.RunCommand("netmgr", "link_info", "--get")
+	if err != nil {
+		return addresses, bosherr.WrapError(err, "Getting link info from netmgr failed")
+	}
+
+	linkSlice := strings.Split(stdout, "\n")
+	var macAddress string
+	var interfaceInfo []string
+	for i, linkInfo := range linkSlice {
+		if i == 0 || i == len(linkSlice)-1 {
+			continue
+		}
+		interfaceInfo = strings.Split(linkInfo, "\t")
+		macAddress = strings.Trim(interfaceInfo[1], " ")
+		interfaceName := strings.Trim(interfaceInfo[0], " ")
+		addresses[macAddress] = interfaceName
+	}
+
+	return addresses, nil
+}
+
+func (net photonosNetManager) ifaceAddresses(staticConfigs []StaticInterfaceConfiguration, dhcpConfigs []DHCPInterfaceConfiguration) ([]boship.InterfaceAddress, []boship.InterfaceAddress) {
+	staticAddresses := []boship.InterfaceAddress{}
+	for _, iface := range staticConfigs {
+		staticAddresses = append(staticAddresses, boship.NewSimpleInterfaceAddress(iface.Name, iface.Address))
+	}
+	dynamicAddresses := []boship.InterfaceAddress{}
+	for _, iface := range dhcpConfigs {
+		dynamicAddresses = append(dynamicAddresses, boship.NewResolvingInterfaceAddress(iface.Name, net.ipResolver))
+	}
+
+	return staticAddresses, dynamicAddresses
+}
---
+++ platform/net/photonos_net_manager_test.go
@@ -0,0 +1,342 @@
+package net_test
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/cloudfoundry/bosh-agent/platform/net"
+	fakearp "github.com/cloudfoundry/bosh-agent/platform/net/arp/fakes"
+	boship "github.com/cloudfoundry/bosh-agent/platform/net/ip"
+	fakeip "github.com/cloudfoundry/bosh-agent/platform/net/ip/fakes"
+	boshsettings "github.com/cloudfoundry/bosh-agent/settings"
+	boshlog "github.com/cloudfoundry/bosh-utils/logger"
+	fakesys "github.com/cloudfoundry/bosh-utils/system/fakes"
+)
+
+var _ = Describe("photonosNetManager", describePhotonosNetManager)
+
+func describePhotonosNetManager() {
+	var (
+		fs                            *fakesys.FakeFileSystem
+		cmdRunner                     *fakesys.FakeCmdRunner
+		ipResolver                    *fakeip.FakeResolver
+		interfaceAddrsProvider        *fakeip.FakeInterfaceAddressesProvider
+		addressBroadcaster            *fakearp.FakeAddressBroadcaster
+		netManager                    Manager
+		interfaceConfigurationCreator InterfaceConfigurationCreator
+	)
+
+	BeforeEach(func() {
+		fs = fakesys.NewFakeFileSystem()
+		cmdRunner = fakesys.NewFakeCmdRunner()
+		ipResolver = &fakeip.FakeResolver{}
+		logger := boshlog.NewLogger(boshlog.LevelNone)
+		interfaceConfigurationCreator = NewInterfaceConfigurationCreator(logger)
+		interfaceAddrsProvider = &fakeip.FakeInterfaceAddressesProvider{}
+		interfaceAddrsValidator := boship.NewInterfaceAddressesValidator(interfaceAddrsProvider)
+		dnsValidator := NewDNSValidator(fs)
+		addressBroadcaster = &fakearp.FakeAddressBroadcaster{}
+		netManager = NewPhotonosNetManager(
+			fs,
+			cmdRunner,
+			ipResolver,
+			interfaceConfigurationCreator,
+			interfaceAddrsValidator,
+			dnsValidator,
+			addressBroadcaster,
+			logger,
+		)
+	})
+
+	writeOutputLine := func(iface string, macAddress string) string {
+		interfacePath := fmt.Sprintf("%s\t%s\tauto      	1500      	up                     \n", iface, macAddress)
+
+		return interfacePath
+	}
+
+	Describe("SetupNetworking", func() {
+		var (
+			dhcpNetwork                           boshsettings.Network
+			staticNetwork                         boshsettings.Network
+			expectedNetworkConfigurationForStatic string
+			expectedNetworkConfigurationForDHCP   string
+			expectedResolveConf                   string
+		)
+
+		BeforeEach(func() {
+			dhcpNetwork = boshsettings.Network{
+				Type:    "dynamic",
+				Default: []string{"dns"},
+				DNS:     []string{"8.8.8.8", "9.9.9.9"},
+				Mac:     "fake-dhcp-mac-address",
+			}
+			staticNetwork = boshsettings.Network{
+				Type:    "manual",
+				IP:      "1.2.3.4",
+				Netmask: "255.255.255.0",
+				Gateway: "3.4.5.6",
+				Mac:     "fake-static-mac-address",
+			}
+			interfaceAddrsProvider.GetInterfaceAddresses = []boship.InterfaceAddress{
+				boship.NewSimpleInterfaceAddress("ethstatic", "1.2.3.4"),
+			}
+			fs.WriteFileString("/etc/resolv.conf", `
+nameserver 8.8.8.8
+nameserver 9.9.9.9
+`)
+
+			expectedResolveConf = `
+[Resolve]
+LLMNR=false
+DNS=8.8.8.8 9.9.9.9
+`
+
+			expectedNetworkConfigurationForStatic = `
+[Match]
+Name=ethstatic
+
+[Network]
+DHCP=no
+IPv6AcceptRA=no
+Address=1.2.3.4/24
+Gateway=3.4.5.6
+`
+
+			expectedNetworkConfigurationForDHCP = `
+[Match]
+Name=ethdhcp
+
+[Network]
+DHCP=ipv4
+IPv6AcceptRA=no
+`
+
+		})
+
+		stubInterfacesWithVirtual := func(physicalInterfaces map[string]boshsettings.Network, virtualInterfaces []string) {
+			interfacePaths := []string{}
+
+			netmgrGetLinkInfo := "Name      	MacAddress       	Mode      	MTU       	State    \n"
+			for iface, networkSettings := range physicalInterfaces {
+				netmgrGetLinkInfo = netmgrGetLinkInfo + writeOutputLine(iface, networkSettings.Mac)
+				interfacePaths = append(interfacePaths, fmt.Sprintf("/etc/systemd/network/00-%s.network", iface))
+			}
+			cmdRunner.AddCmdResult("netmgr link_info --get", fakesys.FakeCmdResult{
+				Stdout:     netmgrGetLinkInfo,
+				Stderr:     "",
+				ExitStatus: 0,
+			})
+			fs.SetGlob("/etc/systemd/network/*", interfacePaths)
+		}
+
+		stubInterfaces := func(physicalInterfaces map[string]boshsettings.Network) {
+			stubInterfacesWithVirtual(physicalInterfaces, nil)
+		}
+
+		It("writes a network script for static and dynamic interfaces", func() {
+			stubInterfaces(map[string]boshsettings.Network{
+				"ethdhcp":   dhcpNetwork,
+				"ethstatic": staticNetwork,
+			})
+
+			cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethstatic --mode static --addr 1.2.3.4/24 --gateway 3.4.5.6", func() {
+				fs.WriteFileString("/etc/systemd/network/00-ethstatic.network", expectedNetworkConfigurationForStatic)
+			})
+
+			cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethdhcp --mode dhcp", func() {
+				fs.WriteFileString("/etc/systemd/network/00-ethdhcp.network", expectedNetworkConfigurationForDHCP)
+			})
+
+			err := netManager.SetupNetworking(boshsettings.Networks{"dhcp-network": dhcpNetwork, "static-network": staticNetwork}, nil)
+			Expect(err).ToNot(HaveOccurred())
+
+			staticConfig := fs.GetFileTestStat("/etc/systemd/network/00-ethstatic.network")
+			Expect(staticConfig).ToNot(BeNil())
+			Expect(staticConfig.StringContents()).To(Equal(expectedNetworkConfigurationForStatic))
+
+			dhcpConfig := fs.GetFileTestStat("/etc/systemd/network/00-ethdhcp.network")
+			Expect(dhcpConfig).ToNot(BeNil())
+			Expect(dhcpConfig.StringContents()).To(Equal(expectedNetworkConfigurationForDHCP))
+		})
+
+		It("returns errors for lesser network devices", func() {
+			err := netManager.SetupNetworking(boshsettings.Networks{"dhcp-network": dhcpNetwork, "static-network": staticNetwork}, nil)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("is greater than the number of network devices"))
+		})
+
+		It("returns errors when it can't create network interface configurations", func() {
+			stubInterfaces(map[string]boshsettings.Network{
+				"ethstatic": staticNetwork,
+			})
+
+			cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethstatic --mode static --addr 1.2.3.4/24 --gateway 3.4.5.6", func() {
+				fs.WriteFileString("/etc/systemd/network/00-ethstatic.network", expectedNetworkConfigurationForStatic)
+			})
+
+			cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethdhcp --mode dhcp", func() {
+				fs.WriteFileString("/etc/systemd/network/00-ethdhcp.network", expectedNetworkConfigurationForDHCP)
+			})
+			staticNetwork.Netmask = "not an ip" //will cause InterfaceConfigurationCreator to fail
+			err := netManager.SetupNetworking(boshsettings.Networks{"static-network": staticNetwork}, nil)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Creating interface configurations"))
+		})
+
+		Context("when manual networks were not configured with proper IP addresses", func() {
+			BeforeEach(func() {
+				interfaceAddrsProvider.GetInterfaceAddresses = []boship.InterfaceAddress{
+					boship.NewSimpleInterfaceAddress("ethstatic", "1.2.3.5"),
+				}
+			})
+
+			It("fails", func() {
+				stubInterfaces(map[string]boshsettings.Network{
+					"ethstatic": staticNetwork,
+				})
+
+				cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethstatic --mode static --addr 1.2.3.4/24 --gateway 3.4.5.6", func() {
+					fs.WriteFileString("/etc/systemd/network/00-ethstatic.network", expectedNetworkConfigurationForStatic)
+				})
+
+				cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethdhcp --mode dhcp", func() {
+					fs.WriteFileString("/etc/systemd/network/00-ethdhcp.network", expectedNetworkConfigurationForDHCP)
+				})
+				errCh := make(chan error)
+				err := netManager.SetupNetworking(boshsettings.Networks{"static-network": staticNetwork}, errCh)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Validating static network configuration"))
+			})
+		})
+
+		It("broadcasts MAC addresses for all interfaces", func() {
+			stubInterfaces(map[string]boshsettings.Network{
+				"ethdhcp":   dhcpNetwork,
+				"ethstatic": staticNetwork,
+			})
+
+			cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethstatic --mode static --addr 1.2.3.4/24 --gateway 3.4.5.6", func() {
+				fs.WriteFileString("/etc/systemd/network/00-ethstatic.network", expectedNetworkConfigurationForStatic)
+			})
+
+			cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethdhcp --mode dhcp", func() {
+				fs.WriteFileString("/etc/systemd/network/00-ethdhcp.network", expectedNetworkConfigurationForDHCP)
+			})
+			errCh := make(chan error)
+			err := netManager.SetupNetworking(boshsettings.Networks{"dhcp-network": dhcpNetwork, "static-network": staticNetwork}, errCh)
+			Expect(err).ToNot(HaveOccurred())
+
+			broadcastErr := <-errCh // wait for all arpings
+			Expect(broadcastErr).ToNot(HaveOccurred())
+
+			Expect(addressBroadcaster.BroadcastMACAddressesAddresses).To(Equal([]boship.InterfaceAddress{
+				boship.NewSimpleInterfaceAddress("ethstatic", "1.2.3.4"),
+				boship.NewResolvingInterfaceAddress("ethdhcp", ipResolver),
+			}))
+
+		})
+
+		It("skips vip networks", func() {
+			stubInterfaces(map[string]boshsettings.Network{
+				"ethdhcp":   dhcpNetwork,
+				"ethstatic": staticNetwork,
+			})
+
+			vipNetwork := boshsettings.Network{
+				Type:    "vip",
+				Default: []string{"dns"},
+				DNS:     []string{"4.4.4.4", "5.5.5.5"},
+				Mac:     "fake-vip-mac-address",
+				IP:      "9.8.7.6",
+			}
+
+			cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethstatic --mode static --addr 1.2.3.4/24 --gateway 3.4.5.6", func() {
+				fs.WriteFileString("/etc/systemd/network/00-ethstatic.network", expectedNetworkConfigurationForStatic)
+			})
+
+			cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethdhcp --mode dhcp", func() {
+				fs.WriteFileString("/etc/systemd/network/00-ethdhcp.network", expectedNetworkConfigurationForDHCP)
+			})
+			err := netManager.SetupNetworking(boshsettings.Networks{
+				"dhcp-network":   dhcpNetwork,
+				"static-network": staticNetwork,
+				"vip-network":    vipNetwork,
+			}, nil)
+			Expect(err).ToNot(HaveOccurred())
+
+			networkConfig := fs.GetFileTestStat("/etc/systemd/network/00-ethstatic.network")
+			Expect(networkConfig).ToNot(BeNil())
+			Expect(networkConfig.StringContents()).To(Equal(expectedNetworkConfigurationForStatic))
+		})
+
+		Context("when no MAC address is provided in the settings", func() {
+			var staticNetworkWithoutMAC boshsettings.Network
+
+			BeforeEach(func() {
+				staticNetworkWithoutMAC = boshsettings.Network{
+					Type:    "manual",
+					IP:      "1.2.3.4",
+					Netmask: "255.255.255.0",
+					Gateway: "3.4.5.6",
+					DNS:     []string{"8.8.8.8", "9.9.9.9"},
+					Default: []string{"dns"},
+				}
+			})
+
+			It("configures network for single device", func() {
+				stubInterfaces(
+					map[string]boshsettings.Network{
+						"ethstatic": staticNetwork,
+					},
+				)
+
+				cmdRunner.SetCmdCallback("netmgr ip4_address --set --interface ethstatic --mode static --addr 1.2.3.4/24 --gateway 3.4.5.6", func() {
+					fs.WriteFileString("/etc/systemd/network/00-ethstatic.network", expectedNetworkConfigurationForStatic)
+				})
+
+				err := netManager.SetupNetworking(boshsettings.Networks{
+					"static-network": staticNetworkWithoutMAC,
+				}, nil)
+				Expect(err).ToNot(HaveOccurred())
+
+				networkConfig := fs.GetFileTestStat("/etc/systemd/network/00-ethstatic.network")
+				Expect(networkConfig).ToNot(BeNil())
+				Expect(networkConfig.StringContents()).To(Equal(expectedNetworkConfigurationForStatic))
+			})
+		})
+	})
+
+	Describe("GetConfiguredNetworkInterfaces", func() {
+		Context("when there are network devices", func() {
+			BeforeEach(func() {
+				netmgrGetLinkInfo := "Name      	MacAddress       	Mode      	MTU       	State    \n"
+				netmgrGetLinkInfo = netmgrGetLinkInfo + writeOutputLine("fake-eth0", "aa:bb")
+				netmgrGetLinkInfo = netmgrGetLinkInfo + writeOutputLine("fake-eth1", "cc:dd")
+
+				cmdRunner.AddCmdResult("netmgr link_info --get", fakesys.FakeCmdResult{
+					Stdout:     netmgrGetLinkInfo,
+					Stderr:     "",
+					ExitStatus: 0,
+				})
+			})
+
+			It("returns networks that are configured by netmgr", func() {
+
+				interfaces, err := netManager.GetConfiguredNetworkInterfaces()
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(interfaces).To(ConsistOf("fake-eth0", "fake-eth1"))
+			})
+		})
+
+		Context("when there are no network devices", func() {
+			It("returns empty list", func() {
+				interfaces, err := netManager.GetConfiguredNetworkInterfaces()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(interfaces).To(Equal([]string{}))
+			})
+		})
+	})
+}
