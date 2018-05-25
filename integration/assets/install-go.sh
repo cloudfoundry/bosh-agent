4a78543f129ac5f44c3e9816d459dc3b94b6ac8e
Luan Santos <lsantos@pivotal.io>
2018-05-25 15:18:02 -0700

    Improve how golang is installed in images
     * lock Vagrant/Windows to use go1.x major version
     * switch Docker image to copy from golang:1
    
    [#156582099](https://www.pivotaltracker.com/story/show/156582099)
    
    Co-authored-by: Danny Berger <dberger@pivotal.io>

--- integration/assets/install-go.sh
+++ integration/assets/install-go.sh
@@ -4,10 +4,12 @@ set -ex
 
 # golang {
 pushd /usr/local
-  apt-get update
-  apt-get install -y jq
-  apt-get clean
-  GO_INFO=$(curl 'https://golang.org/dl/?mode=json' | jq '.[0].files[] | select(.os == "linux" and .arch == "amd64")')
+  curl -Lo /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+  chmod +x /usr/local/bin/jq
+  GO_INFO=$(
+    curl -s 'https://golang.org/dl/?mode=json' | \
+      jq 'map(select(.stable and (.version | split(".")[0] == "go1"))) | .[0].files[] | select(.os == "linux" and .arch == "amd64")'
+  )
   GO_TAR="$(echo "$GO_INFO" | jq -r '.filename')"
   GO_SHA="$(echo "$GO_INFO" | jq -r '.sha256')"
   curl -fSL https://storage.googleapis.com/golang/$GO_TAR -o $GO_TAR
@@ -19,7 +21,7 @@ pushd /usr/local
   export GOPATH=/home/vagrant/go
   export GOROOT=/usr/local/go
 popd
-#}
+# }
 
 chmod -R a+w $GOROOT
 
