96beac1b71ff3fa43e802ce41f9903ae88d85a65
James Myers <jmyers@pivotal.io>
2018-05-25 11:33:43 -0700

    Add pipeline for 2.91.x branch
    [#157608906](https://www.pivotaltracker.com/story/show/157608906)

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+absolute_path() {
+  (cd "$1" && pwd)
+}
+
+scripts_path=$(absolute_path "$(dirname "$0")" )
+
 fly -t production set-pipeline \
-    -p bosh-agent \
-    -c pipeline.yml \
-    --load-vars-from <(lpass show -G "bosh-agent concourse secrets" --notes)
+    -p bosh-agent:2.91.x \
+    -c ${scripts_path}/pipeline.yml \
+    --load-vars-from <(lpass show -G "bosh-agent concourse secrets" --notes) \
+    --var agent_branch=2.91.x \
+    --var version_bump='patch' \
+    --var agent_version_key='agent-2.91-current-version' \
+    --var agent_initial_version=2.91.0
