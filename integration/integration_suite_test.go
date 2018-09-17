6a05db2be3ecbd331eaa4d12cc5e856d872e88dd
James Myers <jmyers@pivotal.io>
2018-09-17 16:48:10 -0700

    Backport: integration: use single ssh connection for all tests
    The overhead of running `vagrant ssh` on every single command being run
    was outrageous. This change opens an SSH connection once at the start of
    the tests and re-uses it thereafter. There are still usages of `vagrant
    ssh` outside the standard `RunCommand` function which could be cleaned
    up too but this is a reasonable start.
    
    This causes the test suite on my workstation to go from 44 minutes to 4
    minutes (11x speedup).
    
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- integration/integration_suite_test.go
+++ integration/integration_suite_test.go
@@ -22,11 +22,13 @@ func TestIntegration(t *testing.T) {
 	BeforeSuite(func() {
 		logger := boshlog.NewLogger(boshlog.LevelDebug)
 		cmdRunner := boshsys.NewExecCmdRunner(logger)
-		testEnvironment = NewTestEnvironment(cmdRunner)
+		var err error
+		testEnvironment, err = NewTestEnvironment(cmdRunner)
+		Expect(err).ToNot(HaveOccurred())
 
 		// Required for reverse-compatibility with older bosh-lite
 		// (remove once a new warden stemcell is built).
-		err := testEnvironment.ConfigureAgentForGenericInfrastructure()
+		err = testEnvironment.ConfigureAgentForGenericInfrastructure()
 		Expect(err).ToNot(HaveOccurred())
 	})
 
