0cb8aed59af2b81074dcc450a996fbd32348ce3f
Belinda Liu <bliu@pivotal.io>
2018-10-05 15:32:18 -0700

    Fix go1.11 gcc issue on windows
    [#160999739](https://www.pivotaltracker.com/story/show/160999739)
    
    Co-authored-by: Josh Russett <jrussett@pivotal.io>

--- ci/tasks/test-unit.ps1
+++ ci/tasks/test-unit.ps1
@@ -4,12 +4,14 @@ trap {
     exit 1
 }
 
+powershell.exe bin/install-go.ps1
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+refreshenv
+
 $env:GOPATH = Join-Path -Path $PWD "gopath"
 $env:PATH = $env:GOPATH + "/bin;C:/go/bin;C:/var/vcap/bosh/bin;" + $env:PATH
 
 cd $env:GOPATH/src/github.com/cloudfoundry/bosh-agent
-
-powershell.exe bin/install-go.ps1
 
 go.exe install github.com/cloudfoundry/bosh-agent/vendor/github.com/onsi/ginkgo/ginkgo
 if ($LASTEXITCODE -ne 0) {
