092a05ee71c5e28d6e0d823bb9bc7bf0480411bd
Beyhan Veli <beyhan.veli@sap.com>
2018-10-25 13:58:03 +0200

    Make fly exec to not ignore bosh-agent binary
    We added the bosh-agent binary to the git index
    so fly execute does not ignore it.
    
    Co-authored-by: Florian Nachtigall <florian.nachtigall@sap.com>

--- bin/repack-stemcell/run.sh
+++ bin/repack-stemcell/run.sh
@@ -10,6 +10,10 @@ cd "${bin}"/../..
 
 mv out/bosh-agent bin/bosh-agent # necessary so that fly -x can be used
 
+git add -f bin/bosh-agent
+
 time fly -t production execute -p -i agent-src=. -o stemcell=out/ -c ./bin/repack-stemcell/task.yml
+
+git reset bin/bosh-agent
 
 ls -la out/stemcell.tgz
