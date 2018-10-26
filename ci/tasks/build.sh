1929b66ac0d04cb677c881bf5674acbe7ab8fc8a
Michael Xu <mxu@pivotal.io>
2018-10-26 11:47:42 -0700

    wip
    Signed-off-by: Sebastian Heid <sebastian.heid@sap.com>

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -8,7 +8,12 @@ export GOPATH=${BASE}/gopath
 
 semver=`cat ${BASE}/version-semver/number`
 
-filename="bosh-agent-${semver}-${GOOS}-${GOARCH}"
+goversion_suffix=""
+if [ ! -z "${GOVERSION}" ]; then
+  goversion_suffix="-${GOVERSION}"
+fi
+
+filename="bosh-agent-${semver}-${GOOS}-${GOARCH}${goversion_suffix}"
 if [[ $GOOS = 'windows' ]]; then
   filename="${filename}.exe"
 fi
@@ -27,16 +32,4 @@ bin/build
 
 shasum -a 256 out/bosh-agent
 
-bosh_agent_path="${BASE}/${DIRNAME}/${filename}"
-cp out/bosh-agent "${bosh_agent_path}"
-
-meta4_path=$BASE/bosh-agent-index/dev/bosh-agent${GOVERSION}/$semver/agent.meta4
-
-mkdir -p "$( dirname "$meta4_path" )"
-meta4 create --metalink="$meta4_path"
-
-meta4 import-file --metalink="$meta4_path" --version="$semver" ${bosh_agent_path}
-meta4 file-set-url --metalink="$meta4_path" --file="${filename}" "https://s3-external-1.amazonaws.com/bosh-agent-binaries/${filename}"
-
-
-cat "$meta4_path"
+cp out/bosh-agent "${BASE}/${DIRNAME}/${filename}"
