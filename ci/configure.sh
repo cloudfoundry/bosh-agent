51faa5bc01567ff904026ca222320ef0bc28a468
James Myers <jmyers@pivotal.io>
2018-10-31 15:46:11 -0700

    Create branch for 2.160.x
    [#161632570](https://www.pivotaltracker.com/story/show/161632570)
    
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 fly -t production set-pipeline \
-    -p bosh-agent \
+    -p "bosh-agent:2.160.x" \
     -c pipeline.yml \
-    --load-vars-from <(lpass show -G "bosh-agent concourse secrets" --notes)
+    --load-vars-from <(lpass show -G "bosh-agent concourse secrets" --notes) \
+    --var agent_branch=2.160.x \
+    --var version_bump='patch' \
+    --var agent_version_key='agent-2.160-current-version' \
+    --var agent_initial_version=2.160.0
