876284ab939376e4f5e5d916833884a8b75c4d4e
Luan Santos <lsantos@pivotal.io>
2018-11-26 16:10:41 -0800

    Allow explict setting of partitioner to sfdisk
    This is a backport of 90e4e50892820753e26a1e1cbbac2dd2f0143f39 from
    branch master.
    
    [finishes #161925673](https://www.pivotaltracker.com/story/show/161925673)

--- platform/disk/linux_disk_manager_test.go
+++ platform/disk/linux_disk_manager_test.go
@@ -61,6 +61,14 @@ var _ = Describe("NewLinuxDiskManager", func() {
 		})
 	})
 
+	Context("when partitioner type is 'sfdisk'", func() {
+		It("returns disk manager configured to use sfdisk", func() {
+			opts := disk.LinuxDiskManagerOpts{PartitionerType: "sfdisk"}
+			diskManager := disk.NewLinuxDiskManager(logger, runner, fs, opts)
+			Expect(diskManager.GetEphemeralDevicePartitioner()).To(Equal(disk.NewSfdiskPartitioner(logger, runner, clock.NewClock())))
+		})
+	})
+
 	Context("when partitioner type is unknown", func() {
 		It("panics", func() {
 			opts := disk.LinuxDiskManagerOpts{PartitionerType: "unknown"}
