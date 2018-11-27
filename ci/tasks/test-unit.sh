822bb2f41b33d702da0d7011acdfe8238d26fa23
Luan Santos <lsantos@pivotal.io>
2018-11-27 14:47:44 -0800

    use bosh user in unit tests
    We have tests which rely on permissions checks which are ignored when
    root.
    
    Co-authored-by: Christopher Brown <cbrown@pivotal.io>

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -4,5 +4,6 @@ set -e
 
 export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
 export GOPATH=$(pwd)/gopath
+chown -R bosh .
 cd gopath/src/github.com/cloudfoundry/bosh-agent
-bin/test-unit
+su bosh -c "env PATH=$PATH GOPATH=$GOPATH bin/test-unit"
