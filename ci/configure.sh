4dfbc7bfb7b3ea6a6cf565f711a5b9cb3f268fb5
Luan Santos <lsantos@pivotal.io>
2018-11-28 10:12:30 -0800

    remove go 1.8 ci jobs
    Go 1.8 was only kept around for Trusty stemcells and we are no longer
    making those.
    
    Co-authored-by: Christopher Brown <cbrown@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+absolute_path() {
+  (cd "$1" && pwd)
+}
+
+scripts_path=$(absolute_path "$(dirname "$0")")
+
 fly -t production set-pipeline \
     -p bosh-agent \
-    -c pipeline.yml \
+    -c $scripts_path/pipeline.yml \
     --load-vars-from <(lpass show -G "bosh-agent concourse secrets" --notes)
