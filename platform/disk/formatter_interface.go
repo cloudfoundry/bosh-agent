16902f005eabe5936cb9f1154966ba43e4a8191a
Pinaki Sarkar <pinaki.sarkar@calculi.com>
2019-01-25 13:32:09 -0800

    Added support for resizing root disks which are of XFS file system types

--- platform/disk/formatter_interface.go
+++ platform/disk/formatter_interface.go
@@ -3,12 +3,15 @@ package disk
 type FileSystemType string
 
 const (
-	FileSystemSwap    FileSystemType = "swap"
-	FileSystemExt4    FileSystemType = "ext4"
-	FileSystemXFS     FileSystemType = "xfs"
-	FileSystemDefault FileSystemType = ""
+	FileSystemSwap             FileSystemType = "swap"
+	FileSystemExt4             FileSystemType = "ext4"
+	FileSystemXFS              FileSystemType = "xfs"
+	FileSystemDefault          FileSystemType = ""
+	FileSystemExtResizeUtility                = "resize2fs"
+	FileSystemXFSResizeUtility                = "xfs_growfs"
 )
 
 type Formatter interface {
 	Format(partitionPath string, fsType FileSystemType) (err error)
+	GetPartitionFormatType(string) (FileSystemType, error)
 }
