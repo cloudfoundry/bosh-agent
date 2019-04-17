14e070e56e7145b3706510cf6a6503b9b750e3f0
Joshua Aresty <joshua.aresty@emc.com>
2019-04-17 16:30:04 -0700

    Fix compilation error
    Co-authored-by: Luan Santos <lsantos@pivotal.io>

--- agentclient/http/agent_client.go
+++ agentclient/http/agent_client.go
@@ -73,7 +73,7 @@ func (c *AgentClient) Start() error {
 	}
 
 	if response.Value != "started" {
-		return bosherr.Errorf("Failed to start agent services with response: '%s'", response)
+		return bosherr.Errorf("Failed to start agent services with response: '%v'", response)
 	}
 
 	return nil
