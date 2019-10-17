f6b53a561e5473490f9d77920859b384b70d71d4
Rebecca Putinski <rputinski@pivotal.io>
2019-10-17 10:22:40 -0700

    Correct the key for sha1 from a compiled package
    [#168818010](https://www.pivotaltracker.com/story/show/168818010)

--- integration/integrationagentclient/agent_client.go
+++ integration/integrationagentclient/agent_client.go
@@ -132,7 +132,7 @@ func (c *IntegrationAgentClient) CompilePackageWithSignedURL(req action.CompileP
 		return map[string]interface{}{}, bosherr.WrapError(err, "Sending 'compile_package' to the agent")
 	}
 
-	sha1, ok := responseValue["sha1_digest"].(string)
+	sha1, ok := responseValue["sha1"].(string)
 	if !ok {
 		return map[string]interface{}{}, bosherr.Errorf("Unable to parse 'compile_package' response from the agent: %#v", responseValue)
 	}
