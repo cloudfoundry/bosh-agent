97aebfdfee4d8f6df137af52d5d68fadc40c3d1d
Tal Usvyatsky <tusvyatsky@pivotal.io>
2019-12-12 12:31:44 -0500

    create skeleton retrying filesystem
    after our discussion in today's mobbing session. we narrowed down to 2 possible decorator approaches:
    - use moveTmpDir for os abstraction, and extract retry-ing out so it can wrap functions
    - create a retrying-filesystem that takes an instance of an fs and delegates most functions,
      but also wraps rename in retry logic.
    
    we are trying (heh) the latter so we can <habeas codeus>
    
    Co-authored-by: Daniel Basner <dbasner@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>
    Co-authored-by: George Gelashvili <ggelashvili+cf@pivotal.io>

---
+++ agent/compiler/retrying_filesystem.go
@@ -0,0 +1,14 @@
+package compiler
+
+// this is a decorator around a bosh utils filesystem instance,
+// that retries at least rename operations
+
+//newFS (takes an fs)
+
+
+
+// func rename () {
+//   wrap retry logic {
+//     super.rename
+//   }
+// }
