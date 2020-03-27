85e73dedd0f4d0c8e3b9e0a29c33d438cbaaef6f
Hanlin Shi <hanlins@vmware.com>
2020-03-26 18:32:05 -0700

    Add unit test to skip the blackhole route
    Signed-off-by: Hanlin Shi <hanlins@vmware.com>

--- platform/net/routes_searcher_unix_test.go
+++ platform/net/routes_searcher_unix_test.go
@@ -43,6 +43,25 @@ default via 172.16.79.1 dev eth0 proto dhcp metric 100
 				}))
 			})
 
+			It("ignores unexpected blackhole routes information", func() {
+				runner.AddCmdResult("ip r", fakesys.FakeCmdResult{
+					Stdout: `172.16.79.0/24 dev eth0 proto kernel scope link metric 100
+169.254.0.0/16 dev eth0 proto link metric 1000
+default via 172.16.79.1 dev eth0 proto dhcp metric 100
+blackhole 10.200.115.192/26  proto bird
+`,
+				})
+
+				routes, err := searcher.SearchRoutes()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(runner.RunCommandsQuietly[0]).To(Equal([]string{"ip", "r"}))
+				Expect(routes).To(Equal([]Route{
+					Route{Destination: "172.16.79.0", Gateway: "0.0.0.0", InterfaceName: "eth0"},
+					Route{Destination: "169.254.0.0", Gateway: "0.0.0.0", InterfaceName: "eth0"},
+					Route{Destination: "0.0.0.0", Gateway: "172.16.79.1", InterfaceName: "eth0"},
+				}))
+			})
+
 			It("ignores empty lines", func() {
 				runner.AddCmdResult("ip r", fakesys.FakeCmdResult{
 					Stdout: `
