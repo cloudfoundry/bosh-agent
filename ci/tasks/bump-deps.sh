b251dc7bc739df3a88feb121e00314ee38cf8ef0
Sam Coward <scoward@pivotal.io>
2021-03-19 11:15:34 -0400

    Bump deps the go mod way

--- ci/tasks/bump-deps.sh
+++ ci/tasks/bump-deps.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-function main() {
-  set -e
+set -e
 
-  git clone bosh-agent bumped-bosh-agent
+git clone bosh-agent bumped-bosh-agent
 
-  mkdir -p workspace/src/github.com/cloudfoundry/
-  ln -s $PWD/bumped-bosh-agent workspace/src/github.com/cloudfoundry/bosh-agent
+cd bumped-bosh-agent
 
-  export GOPATH=$PWD/workspace
+go get -u ./...
+go mod tidy
+go mod vendor
 
-  pushd workspace/src/github.com/cloudfoundry/bosh-agent
-    dep ensure -v -update
-
-    if [ "$(git status --porcelain)" != "" ]; then
-      git status
-      git add vendor Gopkg.lock
-      git config user.name "CI Bot"
-      git config user.email "cf-bosh-eng@pivotal.io"
-      git commit -m "Update vendored dependencies"
-    fi
-  popd
-}
-
-main
+if [ "$(git status --porcelain)" != "" ]; then
+  git status
+  git add vendor go.sum go.mod
+  git config user.name "CI Bot"
+  git config user.email "cf-bosh-eng@pivotal.io"
+  git commit -m "Update vendored dependencies"
+fi
