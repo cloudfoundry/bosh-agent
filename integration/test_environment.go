0925d9142896fbe5ea2eba23e6ba095f79f66a92
Konstantin Kiess <kkiess@vmware.com>
2021-10-04 14:39:57 -0700

    Use bosh ssh instead of ssh as vcap
    - sudo as vcap requires a password which we do not have.
      We have no idea how this has been working
    - Also properly clean up the ssh tunnels
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- integration/test_environment.go
+++ integration/test_environment.go
@@ -541,13 +541,13 @@ func (t *TestEnvironment) StartAgentTunnel(mbusUser, mbusPass string, mbusPort i
 	for i := 1; i < 90; i++ {
 		t.logger.Debug("test environment", "Trying to contact agent via ssh tunnel...")
 		time.Sleep(1 * time.Second)
-		_, err := client.Ping()
+		_, err = client.Ping()
 		if err == nil {
 			break
 		}
 		t.logger.Debug("test environment", err.Error())
 	}
-	return client, nil
+	return client, err
 }
 
 func (t *TestEnvironment) StopAgentTunnel() error {
@@ -555,6 +555,7 @@ func (t *TestEnvironment) StopAgentTunnel() error {
 		return fmt.Errorf("Not running")
 	}
 	t.sshTunnelProc.Wait()
+	t.sshTunnelProc.TerminateNicely(5 * time.Second)
 	t.sshTunnelProc = nil
 	return nil
 }
