0ad4610e528dc0c33830d4bb2852aa6cd43191e5
Joseph Palermo <jpalermo@pivotal.io>
2021-10-04 15:30:55 -0700

    Add pipeline job to build bosh/agent docker image

---
+++ ci/docker/build-args.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eu
+
+cat << EOF > docker-build-args/docker-build-args.json
+{
+  "BOSH_CLI_VERSION": "$(cat bosh-cli-github-release/version)",
+}
+EOF
+
+cat docker-build-args/docker-build-args.json
