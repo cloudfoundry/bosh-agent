2a202c724ba1d349b57c7ca33fff3e14d5989bc1
Joseph Palermo <jpalermo@pivotal.io>
2021-10-04 16:15:21 -0700

    Add credhub cli to dockerfile
    chown bosh home director in dockerfile

--- ci/docker/build-args.sh
+++ ci/docker/build-args.sh
@@ -3,7 +3,8 @@ set -eu
 
 cat << EOF > docker-build-args/docker-build-args.json
 {
-  "BOSH_CLI_VERSION": "$(cat bosh-cli-github-release/version)"
+  "BOSH_CLI_VERSION": "$(cat bosh-cli-github-release/version)",
+  "CREDHUB_CLI_VERSION": "$(cat credhub-cli-github-release/version)"
 }
 EOF
 
