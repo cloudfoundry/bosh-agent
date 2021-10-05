20463bfdd498158bb760c09705803df9e637d52d
Joseph Palermo <jpalermo@pivotal.io>
2021-10-04 17:08:04 -0700

    Create bosh user home director in test unit script

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -6,4 +6,6 @@ export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
 export GOPATH=$(pwd)/gopath
 chown -R bosh .
 cd gopath/src/github.com/cloudfoundry/bosh-agent
+mkdir -p /home/bosh
+chown bosh:bosh /home/bosh
 su bosh -c "env PATH=$PATH GOPATH=$GOPATH bin/test-unit"
