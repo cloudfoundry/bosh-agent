cloudfoundry/bosh-agent

commit eafdb5306593bebcce9febdf401c2f0d4e43b996

Add interfaces and implementations for Nats FW Win
Add code to the agent to secure the remote mbus that
bosh provides. Provide a mechanism to restrict workloads
on the VM other than the agent itself to connect to BOSHs
mbus.

On VM creation bosh provides the Agent on the VM with creds
to connect to Nats. Worklaoads that live on the vm could
extract these credentials and use them to connect to nats and
extract further credentials that are being sent over nats once
the rendering of jobs takes place.

This creates a FW rule within the agent bootstrap to limit which
binaries can create outgoing requests to the nats endpoint.

The created FW rules are ephemeral and exist as long as the agent
process runs. This avoids the necessity for cleaning up FW rules if
the remote address of the nats should change. If a process starts
before the rule exists, the rule will still apply once it exists.

Updated tests relying on SetupNetwork calls with new method signature

platform/net/firewall_provider_linux.go
@@ -1,0 +1,9 @@
+//go:build !windows
+// +build !windows
+
+package net
+
+func SetupNatsFirewall(mbus string) error {
+
+	return nil
+}

commit 9312ec712c3fd253c37316d0416af416a023f029

Fixed the cross-compilation of the 'firewall_provider*.go' files so `func SetupNatsFirewall(mbus string) error {` compiles correctly on all GOOS platforms. Also changed 'bin/test-unit' to explicitly verify compilation succeeds for linux, windows, and darwin GOOS (since we now have 3 distinct GOOS-dependent implementations). Fixes #267.
NOTE: Commit eafdb530 broke the 'bin/test-unit' script (#267) on non-windows, non-linux environments (e.g. mac, 'go version go1.17.3 darwin/arm64'), since `func SetupNatsFirewall` was only being compiled for GOOS=linux and GOOS=windows (because the renamed file was previously being ignored due to its filename (in addition to the embedded "go:build" directives, and with different and conflicting logic than those directives).

NOTE: Since both 'firewall_provider_linux.go' and 'firewall_provider_windows.go' are GOOS-specific implementations, so a 3rd implementation was added (in 'firewall_provider.go') to support all other GOOS values.

NOTE: The 'firewall_provider_linux.go' implementation is compatible only with GOOS=linux. See https://github.com/containerd/cgroups/issues/19 for details.

NOTE: 'firewall_provider_linux_test.go' was renamed to 'firewall_provider_test.go', since it is _not_ GOOS-conditional. (The previous filename implicitly caused the effect as a "//go:build linux" directive.)

After this change, 2 of the 3 'firewall_provider*.go' files get correctly ignored depending on the GOOS (consistent with both the files' names _and_ their embedded "go:build" directives):
```
bosh-agent % GOOS=darwin go list -e -f '{{range .IgnoredGoFiles}}{{printf "%s\n" .}}{{end}}' ./... | grep firewall
firewall_provider_linux.go
firewall_provider_windows.go

bosh-agent % GOOS=windows go list -e -f '{{range .IgnoredGoFiles}}{{printf "%s\n" .}}{{end}}' ./... | grep firewall
firewall_provider.go
firewall_provider_linux.go

bosh-agent % GOOS=linux go list -e -f '{{range .IgnoredGoFiles}}{{printf "%s\n" .}}{{end}}' ./... | grep firewall

platform/net/firewall_provider.go
@@ -1,0 +1,11 @@
+//go:build !windows && !linux
+// +build !windows,!linux
+
+package net
+
+// SetupNatsFirewall is does nothing, except on Linux and Windows
+func SetupNatsFirewall(mbus string) error {
+	// NOTE: If we return a "not supported" err here, unit tests would fail.
+	//return errors.New("not supported")
+	return nil
+}

platform/net/firewall_provider_linux.go
@@ -1,5 +1,5 @@
-//go:build !windows
-// +build !windows
+//go:build linux
+// +build linux
 
 package net
 
@@ -11,6 +11,8 @@ import (
 	"strings"
 
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
+	// NOTE: "cgroups is only intended to be used/compiled on linux based system"
+	// see: https://github.com/containerd/cgroups/issues/19
 	"github.com/containerd/cgroups"
 	"github.com/opencontainers/runtime-spec/specs-go"
 

bin/test-unit
@@ -35,10 +35,16 @@ done
 echo -e "\n Skipping check for unhandled errors until https://github.com/kisielk/errcheck/issues/75 is resolved"
 # $bin/test-unhandled-errors
 
-echo -e "\n Running build script for windows to confirm everything compiles..."
+echo -e "\n Running build script for GOOS=linux to confirm everything compiles..."
+GOOS=linux $bin/build
+
+echo -e "\n Running build script for GOOS=windows to confirm everything compiles..."
 GOOS=windows $bin/build
 
-echo -e "\n Running build script to confirm everything compiles..."
+echo -e "\n Running build script for GOOS=darwin to confirm everything compiles..."
+GOOS=darwin $bin/build
+
+echo -e "\n Running build script (for default GOOS) to confirm everything compiles..."
 $bin/build
 
 echo -e "\n Testing packages..."