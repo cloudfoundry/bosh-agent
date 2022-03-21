81332d0aba97c37b7d8eead7ecdabee0c06d260f
aram price <pricear@vmware.com>
2022-03-21 15:00:35 -0700

    Extract `golangci-lint` from ci/tasks
    - Keep CI concerns separate from running the linter

--- ci/tasks/golangci-lint.sh
+++ ci/tasks/golangci-lint.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-export GOPATH=$(pwd)/gopath
-export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$GOPATH/bin:$PATH
+export GOPATH
+GOPATH="$(pwd)/gopath"
+export PATH=/usr/local/ruby/bin:/usr/local/go/bin:${GOPATH}/bin:${PATH}
 
 cd gopath/src/github.com/cloudfoundry/bosh-agent
 
-go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@$GOLANGCI_LINT_VERSION
-golangci-lint version
-golangci-lint run
+bin/golangci-lint
