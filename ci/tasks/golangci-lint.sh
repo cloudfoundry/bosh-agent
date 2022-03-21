3792e6f270fdc3153cb7a83541f5ed1a34a2e54a
Brian Upton <bupton@vmware.com>
2022-03-21 12:02:15 -0700

    Add gopath bin directory to path and fix directory
    Signed-off-by: Aram Price <pricear@vmware.com>

--- ci/tasks/golangci-lint.sh
+++ ci/tasks/golangci-lint.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
 export GOPATH=$(pwd)/gopath
+export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$GOPATH/bin:$PATH
+
+cd gopath/src/github.com/cloudfoundry/bosh-agent
 
 go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@$GOLANGCI_LINT_VERSION
 golangci-lint version
