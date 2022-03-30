c8d9dbf857d9aab53889194fff4f4db8a8c2e639
Konstantin Kiess <kkiess@vmware.com>
2022-03-30 20:32:53 +0200

    Monit runs on V4 locally, even on a V6 VM
    b700675bbe63c7cc389e3edc2a63f102a7291567 Added handling for
    ipv6 based iptables. Unfortunately it was trying to be too
    smart. The resulting stemcell still broke timing out pinging
    to agent but the logs on the VM showed a different error.
    
    Previously it would fail on adding a rule with a v6 address
    to a v4 table. Now the logs indicated the bosh was sending
    status checks for monit and the agent timed out as the agents
    cgroup only allowed a V6 loopback target. This switches the
    behaviour to always create the monit rule in the v4 table and
    conditionally create the nats rule in the appropriate table

--- platform/net/firewall_provider_linux.go
+++ platform/net/firewall_provider_linux.go
@@ -68,22 +68,14 @@ func SetupNatsFirewall(mbus string) error {
 		return bosherr.WrapError(err, fmt.Sprintf("Error resolving mbus host: %v", host))
 	}
 
-	// default to V4
-	localhostIP := "127.0.0.1"
-	ipVersion := iptables.ProtocolIPv4
-
-	// Check if we're dealing with a V4 Target
-	if addr_array[0].To4() == nil {
-		localhostIP = "0:0:0:0:0:0:0:1"
-		ipVersion = iptables.ProtocolIPv6
-	}
-	ipt, err := iptables.NewWithProtocol(ipVersion)
+	ipt, err := iptables.New()
 	if err != nil {
 		return bosherr.WrapError(err, "Creating Iptables Error")
 	}
-
+	// Even on a V6 VM, Monit will listen to only V4 loopback
+	// First create Monit V4 rules for natsIsolationClassID
 	exists, err := ipt.Exists("mangle", "POSTROUTING",
-		"-d", localhostIP,
+		"-d", "127.0.0.1",
 		"-p", "tcp",
 		"--dport", "2822",
 		"-m", "cgroup",
@@ -95,7 +87,7 @@ func SetupNatsFirewall(mbus string) error {
 	}
 	if !exists {
 		err = ipt.Insert("mangle", "POSTROUTING", 1,
-			"-d", localhostIP,
+			"-d", "127.0.0.1",
 			"-p", "tcp",
 			"--dport", "2822",
 			"-m", "cgroup",
@@ -106,6 +98,18 @@ func SetupNatsFirewall(mbus string) error {
 			return bosherr.WrapError(err, "Iptables Error inserting for monit rule")
 		}
 	}
+
+	// For nats iptables rules we default to V4 unless below dns resolution gives us a V6 target
+	ipVersion := iptables.ProtocolIPv4
+	// Check if we're dealing with a V4 Target
+	if addr_array[0].To4() == nil {
+		ipVersion = iptables.ProtocolIPv6
+	}
+	ipt, err = iptables.NewWithProtocol(ipVersion)
+	if err != nil {
+		return bosherr.WrapError(err, "Creating Iptables Error")
+	}
+
 	err = ipt.AppendUnique("mangle", "POSTROUTING",
 		"-d", host,
 		"-p", "tcp",
@@ -126,6 +130,7 @@ func SetupNatsFirewall(mbus string) error {
 	if err != nil {
 		return bosherr.WrapError(err, "Iptables Error inserting for non-agent DROP rule")
 	}
+
 	var isolationClassID = natsIsolationClassID
 	natsAPICgroup, err := cgroups.New(cgroups.SingleSubsystem(cgroups.V1, cgroups.NetCLS), cgroups.StaticPath("/nats-api-access"), &specs.LinuxResources{
 		Network: &specs.LinuxNetwork{
