84b947528d4c3fc000f8bb76cb4605f762d7a484
Aram Price <pricear@vmware.com>
2022-03-30 14:37:12 -0700

    Remove unset GOPATH and fix home dir ownership
    Signed-off-by: Brian Upton <bupton@vmware.com>

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -4,6 +4,10 @@ set -eu -o pipefail
 CONCOURSE_ROOT="$(pwd)"
 
 chown -R bosh "${CONCOURSE_ROOT}"
+# Concourse explicitly changes ownership of the home directory to root, which prevents
+# creating go cache directories under there.
+chown bosh /home/bosh
+
 cd "${CONCOURSE_ROOT}/bosh-agent"
 
-su bosh -c "env PATH=~bosh/go/bin:$PATH GOPATH=$GOPATH bin/test-unit"
+su bosh -c "env PATH=~bosh/go/bin:$PATH bin/test-unit"
