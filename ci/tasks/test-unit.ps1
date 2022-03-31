fe28b448978c59680ef2173d6dcaad337b167381
Brian Upton <bupton@vmware.com>
2022-03-30 17:38:35 -0700

    Spell ginkgo correctly in error message

--- ci/tasks/test-unit.ps1
+++ ci/tasks/test-unit.ps1
@@ -8,7 +8,7 @@ cd bosh-agent
 
 go.exe run github.com/onsi/ginkgo/ginkgo -r -race -keepGoing -skipPackage="integration,vendor"
 if ($LASTEXITCODE -ne 0) {
-    Write-Host "Gingko returned non-zero exit code: $LASTEXITCODE"
+    Write-Host "Ginkgo returned non-zero exit code: $LASTEXITCODE"
     Write-Error $_
     exit 1
 }
