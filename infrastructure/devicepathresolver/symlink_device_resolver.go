a79778007a6378235229488605b2f95908504bf7
Ned Petrov <71276794+neddp@users.noreply.github.com>
2026-04-30 16:26:24 +0300

    Update infrastructure/devicepathresolver/symlink_device_resolver.go
    Co-authored-by: Ivaylo Ivanov <ivaylogi98@gmail.com>

--- infrastructure/devicepathresolver/symlink_device_resolver.go
+++ infrastructure/devicepathresolver/symlink_device_resolver.go
@@ -63,8 +63,7 @@ func (r *SymlinkDeviceResolver) ResolveSymlinksToDevices(symlinkPattern string) 
 	for _, symlink := range symlinks {
 		absPath, err := r.fs.ReadAndFollowLink(symlink)
 		if err != nil {
-			r.logger.Debug(r.logTag, "Could not resolve symlink %s: %s", symlink, err.Error())
-			continue
+			return nil, bosherr.WrapErrorf(err, "Resolving managed volume symlink '%s'", symlink)
 		}
 
 		r.logger.Debug(r.logTag, "Resolved symlink: %s -> %s", symlink, absPath)
