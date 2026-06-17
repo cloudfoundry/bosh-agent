ea9ce335b247f26a5c0e76f93767fc84007b0091
Matthew Kocher <matthew.kocher@broadcom.com>
2026-06-16 22:48:40 -0700

    move from docker-image to registry-image
    Pushing images with the deprecated docker-image resource
    is broken in recent concourse versions.

--- ci/docker/build-args.sh
+++ ci/docker/build-args.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-cat << EOF > docker-build-args/docker-build-args.json
-{
-  "BOSH_CLI_VERSION": "$(cat bosh-cli-github-release/version)",
-  "CREDHUB_CLI_VERSION": "$(cat credhub-cli-github-release/version)",
-  "JQ_VERSION": "$(cat jq-github-release/version)"
-}
+cat << EOF > docker-build-args/docker-build-args.yml
+BOSH_CLI_VERSION: "$(cat bosh-cli-github-release/version)"
+CREDHUB_CLI_VERSION: "$(cat credhub-cli-github-release/version)"
+JQ_VERSION: "$(cat jq-github-release/version)"
 EOF
 
-cat docker-build-args/docker-build-args.json
+cat docker-build-args/docker-build-args.yml
